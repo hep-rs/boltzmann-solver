1a36019864ecff46cf8afe1df1b45253a157acad
JP-Ellis <josh@jpellis.me>
2018-04-18 10:57:49 +1000

    Force CI to error on warnings
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "nightly", feature(test))]
 #![cfg_attr(feature = "cargo-clippy", allow(unreadable_literal))]
+#![cfg_attr(feature = "strict", deny(missing_docs))]
+#![cfg_attr(feature = "strict", deny(warnings))]
 
 #[macro_use]
 extern crate log;
