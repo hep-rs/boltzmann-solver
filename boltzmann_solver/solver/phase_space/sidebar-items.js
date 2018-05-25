59525801eb32102cae8bb15a0c30a4d8d1f53cf7
Travis CI <travis@travis-ci.org>
2018-05-25 01:58:35 +0000

    Travis build: 38

---
+++ boltzmann_solver/solver/phase_space/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["PhaseSpaceSolver","The solver holding all the information."]]});
---
+++ boltzmann_solver/solver/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["number_density","Solver for the number density evolution given by integrating the Boltzmann equation."],["phase_space","Solver for the phase-space form of the Boltzmann equation."]]});
