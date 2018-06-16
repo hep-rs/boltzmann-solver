afd01048053545c62fa602604bf77768801f7f3c
JP-Ellis <josh@jpellis.me>
2018-06-16 22:34:51 +1000

    Update phase-space solver to use new interface
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/solver/phase_space.rs
+++ src/solver/phase_space.rs
@@ -45,101 +45,27 @@
 //! a)}^2\\)) while leaving all dependence on \\(E_i\\) and \\(\abs{\vt p_i}\\)
 //! explicit.
 
+use super::{ErrorTolerance, Solver, StepChange};
 use ndarray::prelude::*;
+use particle::Particle;
 use statistic::Statistic::{BoseEinstein, FermiDirac};
 use std::f64;
 use universe::Universe;
 
-/// Species
-pub struct Species {
-    spin: u8,
-    mass: f64,
-}
-
-impl Species {
-    /// Create a new species with the specified spin and mass.
-    pub fn new(spin: u8, mass: f64) -> Self {
-        Self { spin, mass }
-    }
-}
-
-struct StepChange {
-    increase: f64,
-    decrease: f64,
-}
-
 /// The solver holding all the information.
-#[allow(dead_code)]
 pub struct PhaseSpaceSolver {
     initialized: bool,
     beta_range: (f64, f64),
-    species: Vec<Species>,
+    particles: Vec<Particle>,
+    interactions: Vec<Box<Fn(&Array2<f64>, f64) -> Array2<f64>>>,
     energies: Array1<f64>,
     energy_steps: usize,
     energy_step_size: f64,
     step_change: StepChange,
+    error_tolerance: ErrorTolerance,
 }
 
 impl PhaseSpaceSolver {
-    /// Create a new instance of the phase space solver.
-    ///
-    /// The default range of temperatures span 1 GeV through to 10^{20} GeV.
-    pub fn new() -> Self {
-        Self {
-            initialized: false,
-            beta_range: (1e-20, 1e0),
-            species: Vec::new(),
-            energies: Array1::zeros(0),
-            energy_steps: 2usize.pow(10),
-            energy_step_size: 0.0,
-            step_change: StepChange {
-                increase: 1.1,
-                decrease: 0.5,
-            },
-        }
-    }
-
-    /// Set the range of inverse temperature values over which the phase space
-    /// is evolved.
-    ///
-    /// Inverse temperature must be provided in units of GeV^{-1}.
-    ///
-    /// This function has a convenience alternative called
-    /// [`PhaseSpaceSolver::temperature_range`] allowing for the limits to be
-    /// specified as temperature in the units of GeV.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the starting value is larger than the final value.
-    pub fn beta_range(mut self, start: f64, end: f64) -> Self {
-        assert!(
-            start < end,
-            "The initial β must be smaller than the final β value."
-        );
-        self.beta_range = (start, end);
-        self
-    }
-
-    /// Set the range of temperature values over which the phase space is
-    /// evolved.
-    ///
-    /// Temperature must be provided in units of GeV.
-    ///
-    /// This function is a convenience alternative to
-    /// [`PhaseSpaceSolver::beta_range`].
-    ///
-    /// # Panics
-    ///
-    /// Panics if the starting value is smaller than the final value.
-    pub fn temperature_range(mut self, start: f64, end: f64) -> Self {
-        assert!(
-            start > end,
-            "The initial temperature must be larger than the final temperature."
-        );
-        self.beta_range = (start.recip(), end.recip());
-        self
-    }
-
     /// Specify the number of energy steps to use in the energy lattice.
     ///
     /// By default, this is set to 2048 steps.
@@ -152,27 +78,50 @@ impl PhaseSpaceSolver {
         self.energy_steps = energy_steps;
         self
     }
+}
 
-    /// Specify the granularity of the way time evolution is done.
-    ///
-    /// The time evolution is done with a step size of \\(h\\) such that
-    /// \\(\beta \to \beta + h\\) in the next step.  As the range of \\(\beta\\)
-    /// spans several orders of magnitude, the step size must be adjusted during
-    /// the time evolution.  This is done by estimating the error at each step
-    /// and if it falls below a particular threshold, the step size is increased
-    /// multiplicatively by \\(h \to h \times \Delta_{+}\\).  Similarly, if the
-    /// estimated error becomes too large, the step size is decreases
-    /// multiplicatively by \\(h \to h \times \Delta_{-}\\).
-    ///
-    /// The value of \\(\Delta_{+}\\) is specified by `increase` and the value
-    /// of \\(\Delta_{-}\\) is specified by `decrease`.  They are `1.1` and
-    /// `0.5` respectively by default.
-    ///
-    /// # Panic
-    ///
-    /// This will panic if the increase factor is not greater than `1.0` or if
-    /// the decrease factor is not less than `1.0`.
-    pub fn step_change(mut self, increase: f64, decrease: f64) -> Self {
+impl Solver for PhaseSpaceSolver {
+    type Solution = Array2<f64>;
+
+    fn new() -> Self {
+        Self {
+            initialized: false,
+            beta_range: (1e-20, 1e0),
+            particles: Vec::with_capacity(20),
+            interactions: Vec::with_capacity(100),
+            energies: Array1::zeros(0),
+            energy_steps: 2usize.pow(10),
+            energy_step_size: 0.0,
+            step_change: StepChange {
+                increase: 1.1,
+                decrease: 0.5,
+            },
+            error_tolerance: ErrorTolerance {
+                upper: 1e-2,
+                lower: 1e-5,
+            },
+        }
+    }
+
+    fn beta_range(mut self, start: f64, end: f64) -> Self {
+        assert!(
+            start < end,
+            "The initial β must be smaller than the final β value."
+        );
+        self.beta_range = (start, end);
+        self
+    }
+
+    fn temperature_range(mut self, start: f64, end: f64) -> Self {
+        assert!(
+            start > end,
+            "The initial temperature must be larger than the final temperature."
+        );
+        self.beta_range = (start.recip(), end.recip());
+        self
+    }
+
+    fn step_change(mut self, increase: f64, decrease: f64) -> Self {
         assert!(
             increase > 1.0,
             "The multiplicative factor to increase the step size must be greater
@@ -187,8 +136,16 @@ impl PhaseSpaceSolver {
         self
     }
 
-    /// Initialize the phase space solver.
-    pub fn initialize(mut self) -> Self {
+    fn error_tolerance(mut self, upper: f64, lower: f64) -> Self {
+        assert!(
+            upper > lower,
+            "The upper error tolerance must be greater than the lower tolerance"
+        );
+        self.error_tolerance = ErrorTolerance { upper, lower };
+        self
+    }
+
+    fn initialize(mut self) -> Self {
         self.energies = Array1::linspace(0.0, 10.0 * self.beta_range.0.recip(), self.energy_steps);
         self.energy_step_size = self.energies[1] - self.energies[0];
         self.initialized = true;
@@ -196,71 +153,20 @@ impl PhaseSpaceSolver {
         self
     }
 
-    /// Add a particle species.
-    ///
-    /// The initial conditions for this particle are generated assuming the
-    /// particle to be in thermal and chemical equilibrium at the initial
-    /// temperature.
-    ///
-    /// If the particle and anti-particle are to be treated separately, the two
-    /// species have to be added.
-    pub fn add_species(&mut self, s: Species) -> &mut Self {
-        self.species.push(s);
+    fn add_particle(&mut self, s: Particle) -> &mut Self {
+        self.particles.push(s);
         self
     }
 
-    /// Create an array containing the initial conditions for all the particle
-    /// species.
-    ///
-    /// All particles species are assumed to be in thermal equilibrium at this
-    /// energy, with the distribution following either the Bose–Einstein or
-    /// Fermi–Dirac distribution as determined by their spin.
-    fn create_initial_conditions(&self) -> Array2<f64> {
-        Array2::from_shape_fn((self.species.len(), self.energies.dim()), |(si, ei)| {
-            let s = &self.species[si];
-            let e = self.energies[ei];
-
-            if s.spin % 2 == 0 {
-                BoseEinstein.phase_space(e, s.mass, 0.0, self.beta_range.0)
-            } else {
-                FermiDirac.phase_space(e, s.mass, 0.0, self.beta_range.0)
-            }
-        })
+    fn add_interaction<F: 'static>(&mut self, f: F) -> &mut Self
+    where
+        F: Fn(&Self::Solution, f64) -> Self::Solution,
+    {
+        self.interactions.push(Box::new(f));
+        self
     }
 
-    /// Compute the energy derivatives.
-    ///
-    /// At the boundaries, the left- and right-sided derivatives are used.  For
-    /// all other cases, a 2-sided derivative is used.
-    ///
-    /// All NaN and infinite values are excluded from the computation so as to
-    /// avoid their propagation to nearby values.
-    fn derivative(&self, phase_spaces: &Array2<f64>) -> Array2<f64> {
-        Array2::from_shape_fn(phase_spaces.dim(), |(si, ei)| {
-            let fm = if ei == 0 {
-                &f64::INFINITY
-            } else {
-                phase_spaces.get([si, ei - 1]).unwrap_or(&f64::INFINITY)
-            };
-            let f = phase_spaces[[si, ei]];
-            let fp = phase_spaces.get([si, ei + 1]).unwrap_or(&f64::INFINITY);
-
-            match (fm.is_finite(), f.is_finite(), fp.is_finite()) {
-                (true, _, true) => (fp - fm) / (2.0 * self.energy_step_size),
-                (false, true, true) => (fp - f) / self.energy_step_size,
-                (true, true, false) => (f - fm) / self.energy_step_size,
-                _ => 0.0,
-            }
-        })
-    }
-
-    /// Evolve the initial conditions by solving the PDEs.
-    ///
-    /// Note that this is not a true PDE solver, but instead converts the PDE
-    /// into an ODE in time (or inverse temperature in this case), with the
-    /// derivative in energy being calculated solely from the previous
-    /// time-step.
-    pub fn solve<U>(&self, universe: &U) -> Array2<f64>
+    fn solve<U>(&self, universe: &U) -> Self::Solution
     where
         U: Universe,
     {
@@ -277,7 +183,7 @@ impl PhaseSpaceSolver {
             if e == 0.0 {
                 0.0
             } else {
-                (e.powi(2) - self.species[si].mass.powi(2)) / e.powi(2)
+                (e.powi(2) - self.particles[si].mass.powi(2)) / e.powi(2)
             }
         });
 
@@ -289,35 +195,56 @@ impl PhaseSpaceSolver {
             n_eval += 1;
 
             // Standard Runge-Kutta integration.
-            let k1 = self.derivative(&y) * &ei_m_on_ei * universe.hubble_rate(beta);
-            let k2 = self.derivative(&(&y + &(&k1 * 0.5))) * &ei_m_on_ei
-                * universe.hubble_rate(beta + 0.5 * h);
-            let k3 = self.derivative(&(&y + &(&k2 * 0.5))) * &ei_m_on_ei
-                * universe.hubble_rate(beta + 0.5 * h);
-            let k4 = self.derivative(&(&y + &k3)) * &ei_m_on_ei * universe.hubble_rate(beta + h);
+            let mut k1 = self.derivative(&y) * &ei_m_on_ei * universe.hubble_rate(beta);
+            k1 = k1 + self.interactions
+                .iter()
+                .map(|f| f(&y, beta))
+                .fold(Self::Solution::zeros(y.dim()), |s, v| s + v);
+            k1 *= h;
+            let tmp = &y + &(&k1 * 0.5);
+            let mut k2 = self.derivative(&tmp) * &ei_m_on_ei * universe.hubble_rate(beta + 0.5 * h);
+            k2 = k2 + self.interactions
+                .iter()
+                .map(|f| f(&tmp, beta + 0.5 * h))
+                .fold(Self::Solution::zeros(y.dim()), |s, v| s + v);
+            k2 *= h;
+            let tmp = &y + &(&k2 * 0.5);
+            let mut k3 = self.derivative(&tmp) * &ei_m_on_ei * universe.hubble_rate(beta + 0.5 * h);
+            k3 = k3 + self.interactions
+                .iter()
+                .map(|f| f(&tmp, beta + 0.5 * h))
+                .fold(Self::Solution::zeros(y.dim()), |s, v| s + v);
+            k3 *= h;
+            let tmp = &y + &k3;
+            let mut k4 = self.derivative(&tmp) * &ei_m_on_ei * universe.hubble_rate(beta + h);
+            k4 = k4 + self.interactions
+                .iter()
+                .map(|f| f(&tmp, beta + h))
+                .fold(Self::Solution::zeros(y.dim()), |s, v| s + v);
+            k4 *= h;
 
             // Calculate dy.  Note that we consume k2, k3 and k4 here.  We use
             // k1 by reference since we need it later to get the error estimate.
-            let dy = (k2 * 2.0 + k3 * 2.0 + k4 + &k1) * h / 6.0;
+            let dy = (k2 * 2.0 + k3 * 2.0 + k4 + &k1) / 6.0;
 
             // Check the error on the RK method vs the Euler method.  If it is
             // small enough, increase the step size.  We use the maximum error
             // for any given element of `dy`.
-            let err = ((k1 * h - &dy) / &y)
+            let err = (k1 / &dy)
                 .iter()
                 .filter(|v| v.is_finite())
-                .map(|v| v.abs())
+                .map(|v| (v - 1.0).abs())
                 .max_by(|a, b| a.partial_cmp(b).unwrap())
                 .expect("Unable to calculate error estimate");
 
             // Adjust the step size as needed based on the step size.
-            if err < 1e-10 {
+            if err < self.error_tolerance.lower {
                 h *= self.step_change.increase;
                 debug!(
                     "Step {:>7}, β = {:>9.2e} -> Increased h to {:.3e}",
                     n_eval, beta, h
                 );
-            } else if err > 1e-3 {
+            } else if err > self.error_tolerance.upper {
                 h *= self.step_change.decrease;
                 debug!(
                     "Step {:>7}, β = {:>9.2e} -> Decreased h to {:.3e}",
@@ -359,6 +286,53 @@ impl Default for PhaseSpaceSolver {
     }
 }
 
+impl PhaseSpaceSolver {
+    /// Create an array containing the initial conditions for all the particle
+    /// species.
+    ///
+    /// All particles species are assumed to be in thermal equilibrium at this
+    /// energy, with the distribution following either the Bose–Einstein or
+    /// Fermi–Dirac distribution as determined by their spin.
+    fn create_initial_conditions(&self) -> Array2<f64> {
+        Array2::from_shape_fn((self.particles.len(), self.energies.dim()), |(si, ei)| {
+            let s = &self.particles[si];
+            let e = self.energies[ei];
+
+            if s.spin % 2 == 0 {
+                BoseEinstein.phase_space(e, s.mass, 0.0, self.beta_range.0)
+            } else {
+                FermiDirac.phase_space(e, s.mass, 0.0, self.beta_range.0)
+            }
+        })
+    }
+
+    /// Compute the energy derivatives.
+    ///
+    /// At the boundaries, the left- and right-sided derivatives are used.  For
+    /// all other cases, a 2-sided derivative is used.
+    ///
+    /// All NaN and infinite values are excluded from the computation so as to
+    /// avoid their propagation to nearby values.
+    fn derivative(&self, phase_spaces: &Array2<f64>) -> Array2<f64> {
+        Array2::from_shape_fn(phase_spaces.dim(), |(si, ei)| {
+            let fm = if ei == 0 {
+                &f64::INFINITY
+            } else {
+                phase_spaces.get([si, ei - 1]).unwrap_or(&f64::INFINITY)
+            };
+            let f = phase_spaces[[si, ei]];
+            let fp = phase_spaces.get([si, ei + 1]).unwrap_or(&f64::INFINITY);
+
+            match (fm.is_finite(), f.is_finite(), fp.is_finite()) {
+                (true, _, true) => (fp - fm) / (2.0 * self.energy_step_size),
+                (false, true, true) => (fp - f) / self.energy_step_size,
+                (true, true, false) => (f - fm) / self.energy_step_size,
+                _ => 0.0,
+            }
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -366,12 +340,12 @@ mod test {
 
     #[test]
     fn no_interaction() {
-        let phi = Species::new(0, 5.0);
+        let phi = Particle::new(0, 5.0);
         let mut solver = PhaseSpaceSolver::new()
             .temperature_range(1e20, 1e-10)
-            .step_change(1.1, 0.5)
+            .error_tolerance(1e-1, 1e-2)
             .initialize();
-        solver.add_species(phi);
+        solver.add_particle(phi);
 
 
 
