6bbf7915cd8b8923e8b81c8d0105b9ee150be23e
JP-Ellis <josh@jpellis.me>
2018-07-02 13:01:49 +1000

    [documentation] Express phase space in terms of number density
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/solver/number_density.rs
+++ src/solver/number_density.rs
@@ -8,6 +8,8 @@
 //! density (or more specifically, the difference in the number densities or a
 //! particle and its corresponding antiparticle).
 //!
+//! # Assumptions
+//!
 //! A couple of assumptions can be made to simplify the Boltzmann equation so
 //! that the number densities can be computed directly.
 //!
@@ -27,10 +29,24 @@
 //! - *Assume \\(\beta \gg E - \mu\\).* In the limit that the temperature is
 //!   much less than \\(E - \mu\\) (or equivalently, that the inverse
 //!   temperature \\(\beta\\) is greater than \\(E - \mu\\)), both the
-//!   Fermi–Dirac and Bose–Einstein approach the Maxwell–Boltzmann distribution:
+//!   Fermi–Dirac and Bose–Einstein approach the Maxwell–Boltzmann distribution,
 //!
 //!   \\begin{equation}
-//!     f_{\textsc{MB}} = \exp[-(E - \mu) \beta]
+//!     f_{\textsc{MB}} = \exp[-(E - \mu) \beta].
+//!   \\end{equation}
+//!
+//!   This simplifies the expression for the number density to
+//!
+//!   \\begin{equation}
+//!     n = \frac{g m^2 K_2(m \beta)}{2 \pi^2 \beta} e^{\mu \beta} = n^{(0)} e^{\mu \beta},
+//!   \\end{equation}
+//!
+//!   where \\(n^{(0)}\\) is the equilibrium number density when \\(\mu = 0\\).
+//!   and conversely allows for the equilibrium phase space distribution to be
+//!   expressed in terms of the \\(\mu = 0\\) distribution:
+//!
+//!   \\begin{equation}
+//!     f_{\textsc{MB}} = e^{\mu \beta} f_{\textsc{MB}}^{(0)} = \frac{n}{n^{(0)}} f_{\textsc{MB}}^{(0)}.
 //!   \\end{equation}
 //!
 //!   Furthermore, the assumption that \\(\beta \gg E - \mu\\) implies that
@@ -44,80 +60,80 @@
 //!         &= - \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right)
 //!                   (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b}) \\\\
 //!         &\quad \times \Biggl[ \abs{\mathcal M(\vt a | \vt b)}^2 \left( \prod_{\vt a} f_i \right)
-//!                             - \abs{\mathcal M(\vt b | \vt a)}^2 \left( \prod_{\vt b} f_i \right) \Biggr],
-//!     \end{aligned}
-//!   \\end{equation}
-//!
-//!   Combined with the assumption that all species are in kinetic equilibrium,
-//!   then the collision term can be further simplified to:
-//!
-//!   \\begin{equation}
-//!     \begin{aligned}
-//!       g_{a_1} \int \vt C[f_{a_{1}}] \frac{\dd \vt p_{a_1}}{(2\pi)^3}
+//!                             - \abs{\mathcal M(\vt b | \vt a)}^2 \left( \prod_{\vt b} f_i \right) \Biggr] \\\\
 //!         &= - \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right)
 //!                   (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b}) \\\\
-//!         &\quad \times \Biggl[ \abs{\mathcal M(\vt a | \vt b)}^2 e^{ \beta \sum_{\vt a} \mu_i }
-//!                             - \abs{\mathcal M(\vt b | \vt a)}^2 e^{ \beta \sum_{\vt b} \mu_i } \Biggr]
-//!                       e^{ - \beta \sum_{\vt a} E_i },
+//!         &\quad \times \Biggl[ \abs{\mathcal M(\vt a | \vt b)}^2 \left( \prod_{\vt a} \frac{n_i}{n_i^{(0)}} f_i^{(0)} \right)
+//!                             - \abs{\mathcal M(\vt b | \vt a)}^2 \left( \prod_{\vt b} \frac{n_i}{n_i^{(0)}} f_i^{(0)} \right) \Biggr].
 //!     \end{aligned}
 //!   \\end{equation}
 //!
-//!   where energy conservation (as imposed by the Dirac delta) is used to
-//!   equate \\(\sum_{\vt a} E_i = \sum_{\vt b} E_i\\).
-//!
-//!   It should be noted that this assumption also simplifies greatly the
-//!   expression for the number density.  In particular, we obtain
+//!   This is then commonly expressed as,
 //!
 //!   \\begin{equation}
-//!     n = e^{\mu \beta} \frac{m^2 K_2(m \beta)}{2 \pi^2 \beta} = e^{\mu \beta} n^{(0)},
-//!   \\end{equation}
-//!
-//!   where \\(n^{(0)}\\) is the equilibrium number density when \\(\mu =
-//!   0\\).
-//!
-//! - *Assume \\(\mathcal{CP}\\) symmetry.* If \\(\mathcal{CP}\\) symmetry is
-//!   assumed, then \\(\abs{\mathcal M(\vt a | \vt b)}^2 \equiv \abs{\mathcal
-//!   M(\vt b | \vt a)}^2\\).  This simplification allows for the exponentials
-//!   of \\(\mu\\) to be taken out of the integral entirely:
-//!
-//!   \\begin{equation}
-//!     g_{a_1} \int \vt C[f_{a_{1}}] \frac{\dd \vt p_{a_1}}{(2\pi)^3}
-//!       = - \left[ e^{ \beta \sum_{\vt a} \mu_i } - e^{ \beta \sum_{\vt b} \mu_i } \right]
-//!            \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
-//!            \abs{\mathcal M(\vt a | \vt b)}^2 e^{ - \beta \sum_{\vt a} E_i }.
-//!   \\end{equation}
-//!
-//!   The remaining integrand is then independent of time and can be
-//!   pre-calculated.  In the case of a \\(2 \leftrightarrow 2\\) interaction,
-//!   this integral is related to the thermally averaged cross-section
-//!   \\(\angles{\sigma v_\text{rel}}\\).
-//!
-//!   Solving the Boltzmann equation is generally required within the context of
-//!   baryogenesis and leptogenesis where the assumption of \\(\mathcal{CP}\\)
-//!   symmetry is evidently not correct.  In such cases, it is convenient to
-//!   define the parameter \\(\epsilon\\) to account for all of the
-//!   \\(\mathcal{CP}\\) asymmetry as.  That is:
-//!
-//!   \\begin{equation}
-//!     \abs{\mathcal M(\vt a | \vt b)}^2 = (1 + \epsilon) \abs{\mathcal{M^{(0)}(\vt a | \vt b)}}^2, \qquad
-//!     \abs{\mathcal M(\vt b | \vt a)}^2 = (1 - \epsilon) \abs{\mathcal{M^{(0)}(\vt a | \vt b)}}^2,
-//!   \\end{equation}
-//!
-//!   where \\(\abs{\mathcal{M^{(0)}}(\vt a | \vt b)}^2\\) is the
-//!   \\(\mathcal{CP}\\)-symmetric squared amplitude.  With \\(\epsilon\\)
-//!   defined as above, the collision term becomes:
-//!
-//!   \\begin{equation}
-//!     \begin{aligned}
 //!       g_{a_1} \int \vt C[f_{a_{1}}] \frac{\dd \vt p_{a_1}}{(2\pi)^3}
-//!         &= - \left[ e^{ \beta \sum_{\vt a} \mu_i } - e^{ \beta \sum_{\vt b} \mu_i } \right] \times
-//!              \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
-//!              \abs{\mathcal M^{(0)}(\vt a | \vt b)}^2 e^{ - \beta \sum_{\vt a} E_i } \\\\
-//!         &\quad - \left[ e^{ \beta \sum_{\vt a} \mu_i } + e^{ \beta \sum_{\vt b} \mu_i } \right] \times
-//!              \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
-//!              \epsilon \abs{\mathcal M^{(0)}(\vt a | \vt b)}^2 e^{ - \beta \sum_{\vt a} E_i }.
-//!     \\end{aligned}
+//!         = - \left( \prod_{\vt a} \frac{n_i}{n_i^{(0)}} \right) \gamma(\vt a | \vt b)
+//!           + \left( \prod_{\vt b} \frac{n_i}{n_i^{(0)}} \right) \gamma(\vt b | \vt a),
 //!   \\end{equation}
+//!
+//!   where we have introduced the interaction density
+//!
+//!   \\begin{equation}
+//!     \gamma(\vt a | \vt b)
+//!       = \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
+//!         \abs{\mathcal M(\vt a | \vt b)}^2 \left( \prod_{\vt a} f^{(0)}_i \right).
+//!   \\end{equation}
+//!
+// //! - *Assume \\(\mathcal{CP}\\) symmetry.* If \\(\mathcal{CP}\\) symmetry is
+// //!   assumed, then \\(\abs{\mathcal M(\vt a | \vt b)}^2 \equiv \abs{\mathcal
+// //!   M(\vt b | \vt a)}^2\\).  This simplification allows for the exponentials
+// //!   of \\(\mu\\) to be taken out of the integral entirely:
+// //!
+// //!   \\begin{equation}
+// //!     g_{a_1} \int \vt C[f_{a_{1}}] \frac{\dd \vt p_{a_1}}{(2\pi)^3}
+// //!       = - \left[ e^{ \beta \sum_{\vt a} \mu_i } - e^{ \beta \sum_{\vt b} \mu_i } \right]
+// //!            \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
+// //!            \abs{\mathcal M(\vt a | \vt b)}^2 e^{ - \beta \sum_{\vt a} E_i }.
+// //!   \\end{equation}
+// //!
+// //!   The remaining integrand is then independent of time and can be
+// //!   pre-calculated.  In the case of a \\(2 \leftrightarrow 2\\) interaction,
+// //!   this integral is related to the thermally averaged cross-section
+// //!   \\(\angles{\sigma v_\text{rel}}\\).
+// //!
+// //!   Solving the Boltzmann equation is generally required within the context of
+// //!   baryogenesis and leptogenesis where the assumption of \\(\mathcal{CP}\\)
+// //!   symmetry is evidently not correct.  In such cases, it is convenient to
+// //!   define the parameter \\(\epsilon\\) to account for all of the
+// //!   \\(\mathcal{CP}\\) asymmetry as.  That is:
+// //!
+// //!   \\begin{equation}
+// //!     \abs{\mathcal M(\vt a | \vt b)}^2 = (1 + \epsilon) \abs{\mathcal{M^{(0)}(\vt a | \vt b)}}^2, \qquad
+// //!     \abs{\mathcal M(\vt b | \vt a)}^2 = (1 - \epsilon) \abs{\mathcal{M^{(0)}(\vt a | \vt b)}}^2,
+// //!   \\end{equation}
+// //!
+// //!   where \\(\abs{\mathcal{M^{(0)}}(\vt a | \vt b)}^2\\) is the
+// //!   \\(\mathcal{CP}\\)-symmetric squared amplitude.  With \\(\epsilon\\)
+// //!   defined as above, the collision term becomes:
+// //!
+// //!   \\begin{equation}
+// //!     \begin{aligned}
+// //!       g_{a_1} \int \vt C[f_{a_{1}}] \frac{\dd \vt p_{a_1}}{(2\pi)^3}
+// //!         &= - \left[ e^{ \beta \sum_{\vt a} \mu_i } - e^{ \beta \sum_{\vt b} \mu_i } \right] \times
+// //!              \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
+// //!              \abs{\mathcal M^{(0)}(\vt a | \vt b)}^2 e^{ - \beta \sum_{\vt a} E_i } \\\\
+// //!         &\quad - \left[ e^{ \beta \sum_{\vt a} \mu_i } + e^{ \beta \sum_{\vt b} \mu_i } \right] \times
+// //!              \int \left( \prod_{\vt a, \vt b} \dd \Pi_i \right) (2 \pi)^4 \delta^4(p_{\vt a} - p_{\vt b})
+// //!              \epsilon \abs{\mathcal M^{(0)}(\vt a | \vt b)}^2 e^{ - \beta \sum_{\vt a} E_i }.
+// //!     \\end{aligned}
+// //!   \\end{equation}
+//!
+//!
+//!
+//!
+//!
+//!
+//!
 
 use super::{ErrorTolerance, Solver, StepChange};
 use ndarray::{prelude::*, FoldWhile, Zip};
