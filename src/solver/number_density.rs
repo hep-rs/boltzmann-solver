6a2c0a5ae7db47e8c8d7bf5f7513318e6cd810b5
JP-Ellis <josh@jpellis.me>
2018-10-10 17:39:46 +1100

    Rework minimal example
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/solver/number_density.rs
+++ src/solver/number_density.rs
@@ -608,7 +608,9 @@ impl NumberDensitySolver {
 #[cfg(test)]
 mod test {
     use super::*;
+    use csv;
     use special_functions::bessel;
+    use std::cell::RefCell;
     use std::f64::consts;
     use universe::StandardModel;
     use utilities::test::*;
@@ -632,46 +634,76 @@ mod test {
         let n1 = Particle::new(1, 1e10);
         let b_minus_l = Particle::new(0, 0.0).set_dof(0.0);
 
-        let yukawa: f64 = 1e-4;
-        let epsilon = 1e-7;
-        let decay_zero_temp = yukawa.powi(2) / (16.0 * consts::PI.powi(2)) * n1.mass;
-
-        let universe = StandardModel::new();
-        let mut solver = NumberDensitySolver::new()
-            .beta_range(1e-14, 1e0)
-            .error_tolerance(1e-1, 1e-2)
-            .normalize_to_photons(true)
-            .initialize();
-
-        solver.add_particle(n1, InitialCondition::Equilibrium(0.0));
-        solver.add_particle(b_minus_l, InitialCondition::Zero);
-
-        // Interaction N ↔ e± H∓ (decay of N, asymmetry in B-L and washout)
-        solver.add_interaction(move |mut s, n, ref c| {
-            let m_beta = n1.mass * c.beta;
-            let dilation_factor = if m_beta > 600.0 {
+        let dilation_factor = |m_beta| {
+            if m_beta > 600.0 {
                 1.0
             } else if m_beta < 1e-15 {
                 0.0
             } else {
                 bessel::k_1(m_beta) / bessel::k_2(m_beta)
-            };
+            }
+        };
 
-            let decay = decay_zero_temp * dilation_factor;
+        let yukawa: f64 = 1e-4;
+        let epsilon = 1e-6;
+        let amplitude_squared = yukawa.powi(2) * n1.mass.powi(2);
 
-            let full_decay = (n[0] - c.eq_n[0]) / (c.hubble_rate * c.beta) * decay;
-            let inverse_decay = decay * c.eq_n[0] / (2.0 * c.eq_fermion);
-            let washout = inverse_decay / (2.0 * c.hubble_rate * c.beta);
+        let universe = StandardModel::new();
+        let mut solver = NumberDensitySolver::new()
+            .beta_range(1e-14, 1e0)
+            .error_tolerance(1e-1, 1e-2)
+            .initialize();
 
-            s[0] -= full_decay;
-            s[1] += epsilon * full_decay;
-            s[1] -= washout * n[1] / (c.hubble_rate * c.beta);
+        let csv = RefCell::new(csv::Writer::from_path("/tmp/minimal_leptogenesis.csv").unwrap());
+        csv.borrow_mut()
+            .serialize((
+                "beta",
+                "n0",
+                "n1",
+                "n0_eq",
+                "n1_eq",
+                "decay",
+                "inverse-decay",
+            )).unwrap();
+
+        // solver.add_particle(n1, InitialCondition::Equilibrium(0.0));
+        solver.add_particle(n1, InitialCondition::Zero);
+        solver.add_particle(b_minus_l, InitialCondition::Zero);
+
+        // Interaction N ↔ e± H∓ (decay of N, asymmetry in B-L and washout)
+        solver.add_interaction(move |mut s, n, ref c| {
+            let unormalized_decay = amplitude_squared / (16.0 * consts::PI)
+                * dilation_factor(n1.mass * c.beta)
+                / (c.hubble_rate * c.beta)
+                / n1.mass;
+
+            // The full n / n(eq) * γ(N → e H)
+            let decay = n[0] * unormalized_decay;
+            // The full γ(e H → N).  We are assuming e and H are always in
+            // equilibrium thus their scaling factors are irrelevant.
+            let inverse_decay = c.eq_n[0] * unormalized_decay;
+            let net_decay = unormalized_decay * (n[0] - c.eq_n[0]);
+
+            s[0] -= net_decay;
+            s[1] += -epsilon * net_decay - 0.5 * n[1] * inverse_decay;
+
+            csv.borrow_mut()
+                .serialize((
+                    c.beta,
+                    n[0],
+                    n[1],
+                    c.eq_n[0],
+                    c.eq_n[1],
+                    decay,
+                    inverse_decay,
+                )).unwrap();
+
             s
         });
 
         let sol = solver.solve(&universe);
 
         assert!(sol[0] < 1e-20);
-        assert!(1e-10 < sol[1] && sol[1] < 1e-5);
+        assert!(1e-10 < sol[1].abs() && sol[1].abs() < 1e-5);
     }
 }
