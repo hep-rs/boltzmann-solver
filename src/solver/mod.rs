7417d1f7f84deba89f754f168ed55c0f4a07aa6f
JP-Ellis <josh@jpellis.me>
2019-01-24 14:56:39 +1100

    Cleanup `solver/mod.rs` by moving small structs into `options.rs`.
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,64 +1,14 @@
 //! Solvers for the Boltzmann equation, or sets of Boltzmann equations
 
-use particle::Particle;
-use universe::Universe;
-
 pub mod number_density;
+mod options;
 pub mod phase_space;
 mod tableau;
 
-struct StepChange {
-    increase: f64,
-    decrease: f64,
-}
-
-impl Default for StepChange {
-    fn default() -> Self {
-        StepChange {
-            increase: 1.1,
-            decrease: 0.5,
-        }
-    }
-}
-
-struct StepPrecision {
-    min: f64,
-    max: f64,
-}
-
-impl Default for StepPrecision {
-    fn default() -> Self {
-        StepPrecision {
-            min: 1e-6,
-            max: 1e-2,
-        }
-    }
-}
-
-struct ErrorTolerance {
-    upper: f64,
-    lower: f64,
-}
-
-impl Default for ErrorTolerance {
-    fn default() -> Self {
-        ErrorTolerance {
-            upper: 1e-2,
-            lower: 1e-5,
-        }
-    }
-}
-
-/// Initial conditions for a particle.
-pub enum InitialCondition {
-    /// The particle's initial density is its equilibrium number with the
-    /// provided value of the chemical potential (in GeV)
-    Equilibrium(f64),
-    /// The particle's initial density begins with a fixed (arbitrary) value.
-    Fixed(f64),
-    /// The particle's initial abundance is zero.
-    Zero,
-}
+pub use self::options::InitialCondition;
+pub(crate) use self::options::{ErrorTolerance, StepChange, StepPrecision};
+use particle::Particle;
+use universe::Universe;
 
 /// Contains all the information relevant to a particular model, including
 /// masses, widths and couplings.  All these attributes can be dependent on the
