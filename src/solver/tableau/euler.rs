63008c5194311e178e9c926c8144b3ad311ac9b8
JP-Ellis <josh@jpellis.me>
2019-03-25 17:47:11 +1100

    Test tableaux using simple ODEs

--- src/solver/tableau/euler.rs
+++
@@ -1,8 +0,0 @@
-#[allow(dead_code)]
-pub const RK_ORDER: usize = 1;
-#[allow(dead_code)]
-pub const RK_A: [[f64; RK_ORDER - 1]; RK_ORDER - 1] = [];
-#[allow(dead_code)]
-pub const RK_B: [[f64; RK_ORDER]; 2] = [[1.0], [1.0]];
-#[allow(dead_code)]
-pub const RK_C: [f64; RK_ORDER - 1] = [];
