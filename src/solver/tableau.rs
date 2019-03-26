9e332e8b8503f46d6c4d5a9b4755fd9319bdb1d0
JP-Ellis <josh@jpellis.me>
2019-03-27 10:05:51 +1100

    Adjust the test integration methods
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/solver/tableau.rs
+++ src/solver/tableau.rs
@@ -37,7 +37,7 @@ pub mod rkf54;
 #[cfg(test)]
 mod tests {
     use crate::utilities::test::*;
-    use ndarray::{array, Array1};
+    use ndarray::{array, prelude::*, FoldWhile, Zip};
     use std::f64::consts::PI;
 
     const TWO_PI: f64 = 2.0 * PI;
@@ -48,7 +48,6 @@ mod tests {
                 mut x: Array1<f64>,
                 mut t: f64,
                 tf: f64,
-                h: f64,
                 f: F,
             ) -> (f64, Array1<f64>)
             where
@@ -61,24 +60,73 @@ mod tests {
                 unsafe {
                     k = std::mem::uninitialized();
                     for ki in &mut k[..] {
-                        std::ptr::write(ki, Array1::zeros(2));
+                        std::ptr::write(ki, Array1::zeros(x.dim()));
                     }
                 };
 
+                let mut h = (tf - t) / 1e3;
+                let h_min = (tf - t) / 1e5;
+                let h_max = (tf - t) / 1e1;
                 while t < tf {
-                    k[0] = f(t, &x) * h;
+                    // Computer each k[i]
+                    k[0] = f(t, &x);
                     for i in 1..RK_S {
                         let t_tmp = t + RK_C[i] * h;
-                        let x_tmp = (0..=i).fold(x.clone(), |total, j| total + &k[j] * RK_A[i][j]);
-                        k[i + 1] = f(t_tmp, &x_tmp) * h;
+                        let ai = RK_A[i];
+                        let x_tmp = (0..i).fold(x.clone(), |total, j| total + h * ai[j] * &k[j]);
+                        k[i] = f(t_tmp, &x_tmp);
                     }
 
                     // Compute the two estimates
                     dx[0] = (0..RK_S).fold(Array1::zeros(2), |total, i| total + &k[i] * RK_B[0][i]);
                     dx[1] = (0..RK_S).fold(Array1::zeros(2), |total, i| total + &k[i] * RK_B[1][i]);
 
-                    x += &dx[0];
-                    t += h;
+                    // Get the error between the estimates
+                    let err = Zip::from(&dx[0])
+                        .and(&dx[1])
+                        .fold_while(0.0, |e, a, b| {
+                            let v = (a - b).abs();
+                            if v > e {
+                                FoldWhile::Continue(v)
+                            } else {
+                                FoldWhile::Continue(e)
+                            }
+                        })
+                        .into_inner();
+
+                    // If the error is within the tolerance, add the result
+                    if err < 1e-5 {
+                        x = x + &dx[0] * h;
+                        t += h;
+                    }
+
+                    // Compute the change in step size based on the current error
+                    let delta = if err > 0.0 {
+                        0.9 * (1e-5 / err).powf(1.0 / f64::from(RK_ORDER + 1))
+                    } else {
+                        1.2
+                    };
+
+                    // And correspondingly adjust the error
+                    h *= if delta < 0.8 {
+                        0.8
+                    } else if delta > 1.2 {
+                        1.2
+                    } else {
+                        delta
+                    };
+
+                    // Make sure the error never becomes *too* big
+                    if h < h_min {
+                        h = h_min;
+                    } else if h > h_max {
+                        h = h_max;
+                    };
+
+                    // And treat the end specially
+                    if t + h > tf {
+                        h = tf - t;
+                    }
                 }
 
                 (t, x)
@@ -98,22 +146,21 @@ mod tests {
             fn $name() {
                 let f = |_t: f64, x: &Array1<f64>| -> Array1<f64> { array![-x[1], x[0]] };
 
-                let h = TWO_PI / 1e5;
                 let x = array![1.0, 0.0];
                 let t = 0.0;
 
-                let (_, x) = $solver(x, t, TWO_PI, h, f);
+                let (_, x) = $solver(x, t, TWO_PI, f);
                 approx_eq(x[0], 1.0, $prec, 0.0);
-                approx_eq(x[1], h, $prec, 0.0);
+                approx_eq(x[1], 0.0, $prec, 10.0f64.powf(-$prec));
             }
         };
     }
 
-    test_sine!(test_sine_bs32, solve_ode_bs32, 7.7);
-    test_sine!(test_sine_ck54, solve_ode_ck54, 7.7);
-    test_sine!(test_sine_dp54, solve_ode_dp54, 7.7);
-    test_sine!(test_sine_dp87, solve_ode_dp87, 7.7);
-    test_sine!(test_sine_rkf54, solve_ode_rkf54, 7.7);
+    test_sine!(test_sine_bs32, solve_ode_bs32, 4.0);
+    test_sine!(test_sine_ck54, solve_ode_ck54, 4.0);
+    test_sine!(test_sine_dp54, solve_ode_dp54, 4.0);
+    test_sine!(test_sine_dp87, solve_ode_dp87, 4.0);
+    test_sine!(test_sine_rkf54, solve_ode_rkf54, 4.0);
 
     macro_rules! test_lotka_volterra {
         ( $name:ident, $solver:ident, $prec:expr ) => {
@@ -123,11 +170,10 @@ mod tests {
                     array![x[0] * (2.0 - x[1]), x[1] * (x[0] - 1.0)]
                 };
 
-                let h = 10.0 / 1e5;
                 let x = array![1.0, 2.7];
                 let t = 0.0;
 
-                let (_, x) = $solver(x, t, 10.0, h, f);
+                let (_, x) = $solver(x, t, 10.0, f);
                 approx_eq(x[0], 0.622_374_063_518_922_9, $prec, 0.0);
                 approx_eq(x[1], 2.115_331_268_162_712_8, $prec, 0.0);
             }
