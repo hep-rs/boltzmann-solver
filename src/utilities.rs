a408bf8d8645deaf77dde56a56496606fcbba3ee
JP-Ellis <josh@jpellis.me>
2019-04-03 10:39:15 +1100

    Add `checked_div` to the `utilities` modules
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,2 +1,20 @@
 #[cfg(test)]
 pub(crate) mod test;
+
+/// Perform a 'checked' division.
+///
+/// Computes the result `a / b` with the following special conditions:
+///
+/// - If `a == 0.0`, returns `0.0` irrespective of the value of `b`;
+/// - If `b == 0.0`, returns `1.0` irrespective of the value of `a` (unless `a
+///   == 0`);
+#[inline]
+pub fn checked_div(a: f64, b: f64) -> f64 {
+    if a == 0.0 {
+        0.0
+    } else if b == 0.0 {
+        1.0
+    } else {
+        a / b
+    }
+}
