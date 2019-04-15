7b393ee0b7cad5b037883cc4e0f8291c7313e453
JP-Ellis <josh@jpellis.me>
2019-04-15 19:54:52 +1000

    Reorganize minimal leptogenesis to be more modular
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- tests/minimal_leptogenesis/main.rs
+++ tests/minimal_leptogenesis/main.rs
@@ -18,20 +18,8 @@ fn minimal_leptogenesis() {
     // Setup the directory for CSV output
     ::std::fs::create_dir("/tmp/minimal_leptogenesis/").unwrap_or(());
 
-    let model = VanillaLeptogenesisModel::new(1e-15);
-
-    let sol = solve::solve(model);
-
-    assert!(1e-10 < sol[0].abs() && sol[0].abs() < 1e-5);
-    assert!(sol[1] < 1e-20);
-}
-
-#[test]
-fn scan_minimal_leptogenesis() {
-    // Setup the directory for CSV output
-    ::std::fs::create_dir("/tmp/minimal_leptogenesis/").unwrap_or(());
-
-    let model = VanillaLeptogenesisModel::new(1e-15);
+    let mut model = VanillaLeptogenesisModel::new(1e-15);
+    model.coupling.y_v = model.coupling.y_v * 1e-4;
 
     let sol = solve::solve(model);
 
