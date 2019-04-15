9549716707d9dce033e3924f8bd561581342ffc8
JP-Ellis <josh@jpellis.me>
2019-04-15 20:44:44 +1000

    Fix unused import

--- src/solver_ap/tableau.rs
+++ src/solver_ap/tableau.rs
@@ -36,8 +36,8 @@ pub mod rkf54;
 
 #[cfg(test)]
 mod tests {
+    use crate::solver_ap::DEFAULT_WORKING_PRECISION as DWP;
     use crate::utilities::test::*;
-    use crate::{solver_ap::DEFAULT_WORKING_PRECISION as DWP, utilities::test::*};
     use ndarray::{array, prelude::*, FoldWhile, Zip};
     use rug::{ops::*, Float};
     use std::{f64::consts::PI, iter};
