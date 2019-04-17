16818b31e1d30b92df7a41d725cc3856bc040711
JP-Ellis <josh@jpellis.me>
2019-04-17 11:00:19 +1000

    Fix and optimize scan
    Also make it by default very small so that the CI works

--- tests/minimal_leptogenesis_scan/main.rs
+++ tests/minimal_leptogenesis_scan/main.rs
@@ -4,6 +4,7 @@ extern crate itertools;
 extern crate ndarray;
 extern crate num;
 extern crate quadrature;
+extern crate rayon;
 extern crate rgsl;
 extern crate special_functions;
 
@@ -11,38 +12,38 @@ mod model;
 mod solve;
 
 use boltzmann_solver::solver::Model;
+use itertools::iproduct;
 use model::VanillaLeptogenesisModel;
 use ndarray::prelude::*;
-use ndarray_parallel::prelude::*;
+use rayon::prelude::*;
+use std::sync::RwLock;
 
 #[test]
 fn minimal_leptogenesis_scan() {
     // Setup the directory for CSV output
     ::std::fs::create_dir("/tmp/minimal_leptogenesis/").unwrap_or(());
 
-    let mut csv = csv::Writer::from_path("/tmp/minimal_leptogenesis/scan.csv").unwrap();
-    csv.serialize(("y", "m", "B-L")).unwrap();
+    let csv = RwLock::new(csv::Writer::from_path("/tmp/minimal_leptogenesis/scan.csv").unwrap());
+    csv.write().unwrap().serialize(("y", "m", "B-L")).unwrap();
 
-    for &y in &Array1::linspace(-8.0, -4.0, 2) {
-        let y = 10.0f64.powf(y);
+    let ym: Vec<_> = iproduct!(
+        Array1::linspace(-8.0, -4.0, 2).into_iter(),
+        Array1::linspace(6.0, 14.0, 2).into_iter()
+    )
+    .map(|(&y, &m)| (10.0f64.powf(y), 10.0f64.powf(m)))
+    .collect();
 
-        let ms: Vec<_> = Array1::linspace(-4.0, 4.0, 3)
-            .par_iter()
-            .map(|&m| {
-                let m = 10.0f64.powf(m);
-                let model = VanillaLeptogenesisModel::new(1e-17);
-                let sol = solve::solve(model, y, m);
-                (m, sol[0])
-            })
-            .collect();
+    ym.into_par_iter().for_each(|(y, m)| {
+        let model = VanillaLeptogenesisModel::new(1e-17);
+        let sol = solve::solve(model, y, m);
 
-        for (m, bl) in &ms {
-            csv.serialize((
+        csv.write()
+            .unwrap()
+            .serialize((
                 format!("{:.10e}", y),
                 format!("{:.10e}", m),
-                format!("{:.10e}", bl),
+                format!("{:.10e}", sol[0]),
             ))
             .unwrap();
-        }
-    }
+    });
 }
