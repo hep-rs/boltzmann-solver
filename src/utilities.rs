f963f8803b1f627a39666415e8c76686d2f5982c
JP-Ellis <josh@jpellis.me>
2019-04-30 18:52:08 +1000

    Add helper function to calculate t_min and t_max
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/utilities.rs
+++ src/utilities.rs
@@ -42,3 +42,136 @@ pub fn checked_div_ap(a: &Float, b: &Float) -> Float {
         Float::with_val(a.prec(), a / b)
     }
 }
+
+/// Return the minimum and maximum value of the Mandelstam variable \\(t\\)
+/// based on the four particle masses \\(m_1\\), \\(m_2\\), \\(m_3\\) and
+/// \\(m_4\\), where particles 1 and 2 are initial state and particles 3 and 4
+/// are the final state particles.
+///
+/// The values are given by:
+/// \\begin{equation}
+///   t_{\text{min},\text{max}} = a \pm b
+/// \\end{equation}
+/// where
+/// \\begin{equation}
+///   \\begin{aligned}
+///     a &= \frac{1}{2} \left( m_1^2 + m_2^2 + m_3^2 + m_4^2 - s - \frac{\left( m_1^2 - m_2^2 \right) \left( m_3^2 - m_4^2 \right)}{s} \right) \\\\
+///     b &= \frac{\mathop{\text{sign}}\left(m_1^2 - m_2^2\right)}{2s}
+///           \sqrt{m_2^2 + \left(s - m_1^2\right)^2 - 2 m_2^2 \left(s + m_1^2\right)}
+///           \sqrt{m_4^2 + \left(s - m_3^2\right)^2 - 2 m_4^2 \left(s + m_3^2\right)}
+///   \\end{aligned}
+/// \\end{equation}
+pub fn t_min_max(s: f64, m1: f64, m2: f64, m3: f64, m4: f64) -> (f64, f64) {
+    let m1 = m1.powi(2);
+    let m2 = m2.powi(2);
+    let m3 = m3.powi(2);
+    let m4 = m4.powi(2);
+
+    debug_assert!(s >= m1 + m2, "s must be greater than m1^2 + m2^2.");
+    debug_assert!(s >= m3 + m4, "s must be greater than m3^2 + m4^2.");
+
+    if s == 0.0 {
+        return (-0.0, 0.0);
+    }
+
+    let t_const = (m1 + m2 + m3 + m4 - s - (m1 - m2) * (m3 - m4) / s) / 2.0;
+    let t_cos = ((m2.powi(2) + (s - m1).powi(2) - 2.0 * m2 * (s + m1))
+        * (m4.powi(2) + (s - m3).powi(2) - 2.0 * m4 * (s + m3)))
+        .abs()
+        .sqrt()
+        / (2.0 * s);
+
+    (t_const - t_cos, t_const + t_cos)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::t_min_max;
+    use crate::utilities::test::*;
+    use ndarray::prelude::*;
+
+    #[test]
+    fn t_min_max_zero() {
+        let (m1, m2, m3, m4): (f64, f64, f64, f64) = (0.0, 0.0, 0.0, 0.0);
+
+        let s_min = m1.powi(2) + m2.powi(2);
+        let ss = Array1::linspace(s_min, s_min + 1e3, 100);
+
+        for &s in &ss {
+            let (t_min, t_max) = t_min_max(s, m1, m2, m3, m4);
+
+            approx_eq(t_min, -s, 8.0, 1e-8);
+            approx_eq(t_max, 0.0, 8.0, 1e-8);
+        }
+    }
+
+    #[test]
+    fn t_min_max_one() {
+        // m1 != 0
+        let (m1, m2, m3, m4): (f64, f64, f64, f64) = (100.0, 0.0, 0.0, 0.0);
+
+        let s_min = m1.powi(2) + m2.powi(2);
+        let ss = Array1::linspace(s_min, s_min + 1e3, 100);
+
+        for &s in &ss {
+            let (t_min, t_max) = t_min_max(s, m1, m2, m3, m4);
+
+            approx_eq(t_min, m1.powi(2) - s, 8.0, 1e-8);
+            approx_eq(t_max, 0.0, 8.0, 1e-8);
+        }
+
+        // m2 != 0
+        let (m1, m2, m3, m4): (f64, f64, f64, f64) = (0.0, 100.0, 0.0, 0.0);
+
+        let s_min = m1.powi(2) + m2.powi(2);
+        let ss = Array1::linspace(s_min, s_min + 1e3, 100);
+
+        for &s in &ss {
+            let (t_min, t_max) = t_min_max(s, m1, m2, m3, m4);
+
+            approx_eq(t_min, m2.powi(2) - s, 8.0, 1e-8);
+            approx_eq(t_max, 0.0, 8.0, 1e-8);
+        }
+
+        // m3 != 0
+        let (m1, m2, m3, m4): (f64, f64, f64, f64) = (0.0, 0.0, 100.0, 0.0);
+
+        let s_min = m3.powi(2) + m4.powi(2);
+        let ss = Array1::linspace(s_min, s_min + 1e3, 100);
+
+        for &s in &ss {
+            let (t_min, t_max) = t_min_max(s, m1, m2, m3, m4);
+
+            approx_eq(t_min, m3.powi(2) - s, 8.0, 1e-8);
+            approx_eq(t_max, 0.0, 8.0, 1e-8);
+        }
+
+        // m4 != 0
+        let (m1, m2, m3, m4): (f64, f64, f64, f64) = (0.0, 0.0, 0.0, 100.0);
+
+        let s_min = m3.powi(2) + m4.powi(2);
+        let ss = Array1::linspace(s_min, s_min + 1e3, 100);
+
+        for &s in &ss {
+            let (t_min, t_max) = t_min_max(s, m1, m2, m3, m4);
+
+            approx_eq(t_min, m4.powi(2) - s, 8.0, 1e-8);
+            approx_eq(t_max, 0.0, 8.0, 1e-8);
+        }
+    }
+
+    #[test]
+    fn t_min_max_multiple() {
+        let (m1, m2, m3, m4): (f64, f64, f64, f64) = (10.0, 20.0, 30.0, 40.0);
+
+        let s_min = m3.powi(2) + m4.powi(2);
+        let ss = Array1::linspace(s_min, s_min + 1e3, 100);
+
+        for &s in &ss {
+            let (t_min, t_max) = t_min_max(s, m1, m2, m3, m4);
+
+            assert!(t_min.is_finite() && t_max.is_finite());
+            assert!(t_min <= t_max);
+        }
+    }
+}
