4744229027184da7f56427088bc9b0834afaeaf1
JP-Ellis <josh@jpellis.me>
2019-06-18 19:26:17 +1000

    Use Fern for logging
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- tests/leptogenesis.rs
+++ tests/leptogenesis.rs
@@ -2,13 +2,40 @@
 //! leptogenesis in the standard type-I seesaw.
 
 extern crate boltzmann_solver;
+extern crate chrono;
 extern crate csv;
+extern crate fern;
 extern crate itertools;
 extern crate ndarray;
 extern crate num;
 extern crate quadrature;
 extern crate rgsl;
 extern crate special_functions;
+
+use std::io;
+
+/// Setup logging
+fn setup_logging() {
+    let mut base_config = fern::Dispatch::new();
+
+    base_config = base_config
+        .level(log::LevelFilter::Info)
+        .level_for("overly-verbose-target", log::LevelFilter::Warn);
+
+    let stderr_config = fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{} {} {} - {}",
+                chrono::Local::now().format("%H:%M:%S%.3f"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .chain(io::stderr());
+
+    base_config.chain(stderr_config).apply().unwrap();
+}
 
 pub mod leptogenesis_sp;
 
