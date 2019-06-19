7fc9661898b01021bf2f08ab85eb53a2ac0ede1a
JP-Ellis <josh@jpellis.me>
2019-06-19 11:27:50 +1000

    Create a more general output directory

--- tests/leptogenesis.rs
+++ tests/leptogenesis.rs
@@ -12,15 +12,13 @@ extern crate quadrature;
 extern crate rgsl;
 extern crate special_functions;
 
-use std::io;
+use std::{env::temp_dir, fs, io, path::PathBuf};
 
 /// Setup logging
 fn setup_logging() {
     let mut base_config = fern::Dispatch::new();
 
-    base_config = base_config
-        .level(log::LevelFilter::Info)
-        .level_for("overly-verbose-target", log::LevelFilter::Warn);
+    base_config = base_config.level(log::LevelFilter::Info);
 
     let stderr_config = fern::Dispatch::new()
         .format(|out, message, record| {
@@ -34,7 +32,28 @@ fn setup_logging() {
         })
         .chain(io::stderr());
 
-    base_config.chain(stderr_config).apply().unwrap();
+    base_config.chain(stderr_config).apply().unwrap_or(());
+}
+
+/// Output directory
+fn output_dir() -> PathBuf {
+    let mut dir = temp_dir();
+    dir.push("leptogenesis");
+    if !dir.is_dir() {
+        log::info!("Creating output directory: {}", dir.display());
+    }
+
+    for subdir in &["sp", "ap"] {
+        match fs::create_dir_all(dir.join(subdir)) {
+            Ok(()) => (),
+            Err(e) => {
+                log::error!("Unable to created directory: {}", e);
+                panic!()
+            }
+        }
+    }
+
+    dir
 }
 
 pub mod leptogenesis_sp;
