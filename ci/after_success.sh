d6794253908647e6497ea1b579325e65694efb5b
JP-Ellis <josh@jpellis.me>
2019-09-10 19:10:13 +1000

    Switch to cargo tarpaulin
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -7,36 +7,16 @@ set -o nounset
 # Exit on any error
 set -o errexit
 
-# We only need to run the coverage suite once
-COVERAGE_RUN=false
-
-run_kcov() {
-    # Run kcov on all the test suites
-    if [[ $COVERAGE_RUN != "true" ]]; then
-        cargo coveralls --features "$FEATURES"
-        COVERAGE_RUN=true
-    fi
-}
-
-coverage_codecov() {
+coverage() {
     if [[ "$TRAVIS_RUST_VERSION" != "stable" ]]; then
         return
     fi
 
-    run_kcov
-
-    bash <(curl -s https://codecov.io/bash) -s target/kcov
+    cargo tarpaulin --features "$FEATURES" \
+          --out Xml \
+          --ciserver travis-ci --coveralls $TRAVIS_JOB_ID
+    bash <(curl -s https://codecov.io/bash)
     echo "Uploaded code coverage to codecov.io"
-}
-
-coverage_coveralls() {
-    if [[ "$TRAVIS_RUST_VERSION" != "stable" ]]; then
-        return
-    fi
-
-    run_kcov
-
-    # Data is automatically uploaded by kcov
 }
 
 git_setup() {
@@ -94,8 +74,7 @@ EOF
 }
 
 main() {
-    coverage_coveralls
-    coverage_codecov
+    coverage
     make_doc
 }
 
