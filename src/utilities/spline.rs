0abac2827d66df56e719c465dbcf7ee320703d7a
JP-Ellis <josh@jpellis.me>
2019-11-21 14:47:49 +1100

    Update spline documentation and add benchmarks
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/utilities/spline.rs
+++ src/utilities/spline.rs
@@ -1,11 +1,20 @@
 //! Cubic Hermite interpolation
 
-/// Cubic Hermite spline interpolator using constant data
-pub(crate) struct ConstCubicHermiteSpline {
-    data: &'static [(f64, f64, f64)],
+/// Cubic Hermite spline interpolator using a constant data array
+pub struct ConstCubicHermiteSpline {
+    /// Data array arranged in triples of `(xi, yi, mi)` where `xi`, `yi` are x
+    /// and y values of a particular point, and `mi` is the gradient for that
+    /// point.
+    ///
+    /// The data array *must* be sorted in increasing `x` values.
+    pub data: &'static [(f64, f64, f64)],
 }
 
 impl ConstCubicHermiteSpline {
+    /// Sample the spline at the specific `x` value.
+    ///
+    /// For values of `x` outside of the domain of the underlying data, the
+    /// boundary value is returned.
     pub fn sample(&self, x: f64) -> f64 {
         match self
             .data
@@ -31,10 +40,17 @@ impl ConstCubicHermiteSpline {
 }
 
 /// Cubic Hermite spline interpolator
-pub(crate) struct CubicHermiteSpline {
+pub struct CubicHermiteSpline {
+    // `(x, y)` pairs through which the spline goes through
     data: Vec<(f64, f64)>,
+    // Gradient at each point.
     gradients: Vec<f64>,
+    // Segments where the spline is deemed to be accurate.  The value of
+    // `accurate[i]` determines the accuracy for the interval `x[i]` to
+    // `x[i+1]`.
     accurate: Vec<bool>,
+    // Number of points required before it begins to consider whether an
+    // interval is accurate.
     min_points: usize,
 }
 
@@ -49,6 +65,7 @@ impl CubicHermiteSpline {
         }
     }
 
+    /// Return the number of data points in the underlying data
     pub fn len(&self) -> usize {
         self.data.len()
     }
@@ -65,7 +82,7 @@ impl CubicHermiteSpline {
     /// If the data point is within the range that is being interpolated, the
     /// given value is compared to the interpolated value and if they are in
     /// good agreement, both sides adjacent to the new point are marked as
-    /// accurate.
+    /// accurate (provided the spline has reached the minimum number of points).
     pub fn add(&mut self, x: f64, y: f64) {
         match self
             .data
@@ -100,8 +117,8 @@ impl CubicHermiteSpline {
                 self.compute_gradient(i + 1);
 
                 let delta = (ny - y).abs();
-                if self.data.len() > self.min_points / 2
-                    && (delta / (ny.abs() + y.abs()) < 0.05 || delta < 1e-6)
+                if self.data.len() > self.min_points
+                    && (delta / (ny.abs() + y.abs()) < 0.05 || delta < 1e-3)
                 {
                     self.accurate[i - 1] = true;
                     self.accurate[i] = true;
@@ -127,22 +144,6 @@ impl CubicHermiteSpline {
         }
     }
 
-    // fn compute_gradients(&mut self) {
-    //     let n = self.data.len();
-    //     if n < 2 {
-    //         return;
-    //     }
-
-    //     self.gradients[0] = self.data[1].1 - self.data[0].1;
-    //     for i in 1..(n - 1) {
-    //         let (x0, y0) = self.data[i - 1];
-    //         let (x1, y1) = self.data[i];
-    //         let (x2, y2) = self.data[i + 1];
-    //         self.gradients[i] = 0.5 * ((y2 - y1) / (x2 - x1) + (y1 - y0) / (x1 - x0)) * (x2 - x1)
-    //     }
-    //     self.gradients[n - 1] = self.data[n - 1].1 - self.data[n - 2].1;
-    // }
-
     /// Check whether a given interval is accurate or not.
     ///
     /// If the point checked falls outside of the boundary, this is
@@ -161,6 +162,10 @@ impl CubicHermiteSpline {
         }
     }
 
+    /// Sample the spline at the specific `x` value.
+    ///
+    /// For values of `x` outside of the domain of the underlying data, the
+    /// boundary value is returned.
     pub fn sample(&self, x: f64) -> f64 {
         match self
             .data
@@ -187,8 +192,13 @@ impl CubicHermiteSpline {
     }
 }
 
-pub(crate) fn rec_geomspace(start: f64, end: f64, pow: u32) -> Vec<f64> {
-    let mut v = Vec::with_capacity(2usize.pow(pow));
+/// Create a recursively generated geometrically spaced interval between `start`
+/// and `end`.
+///
+/// This is the analogous version of the recursively generated linearly spaced
+/// interval [`rec_linspace`].
+pub fn rec_geomspace(start: f64, end: f64, recursions: u32) -> Vec<f64> {
+    let mut v = Vec::with_capacity(2usize.pow(recursions));
 
     v.push(start);
     v.push(end);
@@ -197,7 +207,7 @@ pub(crate) fn rec_geomspace(start: f64, end: f64, pow: u32) -> Vec<f64> {
     let end = end.ln();
 
     let mut base = 2.0;
-    for i in 2..2u64.pow(pow) {
+    for i in 2..2u64.pow(recursions) {
         let i = i as f64;
         if i > base {
             base *= 2.0;
@@ -211,15 +221,31 @@ pub(crate) fn rec_geomspace(start: f64, end: f64, pow: u32) -> Vec<f64> {
     v
 }
 
-#[allow(unused)]
-pub(crate) fn rec_linspace(start: f64, end: f64, pow: u32) -> Vec<f64> {
-    let mut v = Vec::with_capacity(2usize.pow(pow));
+/// Create a recursively generated linearly spaced interval spanning `start` and
+/// `end` inclusively.
+///
+/// The resulting vector will have values in the following order:
+///
+/// \\begin{equation}
+///   \left[
+///     a,
+///     b,
+///     \frac{a + b}{2},
+///     \frac{3a + b}{4}, \frac{a + 3b}{4},
+///     \frac{7a + b}{8}, \frac{5a + 3b}{8}, \frac{3a + 5b}{8}, \frac{a + 8b}{8},
+///     \dots
+///   \right]
+/// \\end{equation}
+///
+/// The number of recursions is determined by `recursions`.
+pub fn rec_linspace(start: f64, end: f64, recursions: u32) -> Vec<f64> {
+    let mut v = Vec::with_capacity(2usize.pow(recursions));
 
     v.push(start);
     v.push(end);
 
     let mut base = 2.0;
-    for i in 2..2u64.pow(pow) {
+    for i in 2..2u64.pow(recursions) {
         let i = i as f64;
         if i > base {
             base *= 2.0;
@@ -238,7 +264,9 @@ mod test {
     use ndarray::prelude::*;
     use std::{f64, fs, io::prelude::*, io::BufWriter};
 
-    fn f(x: f64) -> f64 {
+    pub(crate) const RECURSIONS: u32 = 12;
+
+    pub(crate) fn f(x: f64) -> f64 {
         x * (x - 1.0) * (x - 0.2) * (x - 0.5) * f64::exp(-3.0 * f64::exp(x))
     }
 
@@ -249,20 +277,77 @@ mod test {
         let mut path = std::env::temp_dir();
         path.push("sampled.csv");
         let mut sampled = BufWriter::new(fs::File::create(path).unwrap());
-        for x in super::rec_linspace(0.0, 1.0, 14) {
+        for x in super::rec_linspace(0.0, 1.0, RECURSIONS) {
             if !spline.accurate(x) {
                 writeln!(sampled, "{:e},{:e}", x, f(x)).unwrap();
                 spline.add(x, f(x))
             }
         }
 
-        println!("Points in spline: {}", spline.len());
-
         let mut path = std::env::temp_dir();
         path.push("spline.csv");
         let mut output = BufWriter::new(fs::File::create(path).unwrap());
-        for &x in Array1::linspace(0.0, 1.0, 2048).iter() {
+        for &x in Array1::linspace(0.0, 1.0, 2usize.pow(RECURSIONS)).iter() {
             writeln!(output, "{:e},{:e}", x, spline.sample(x)).unwrap();
         }
+    }
+}
+
+#[cfg(all(test, feature = "nightly"))]
+mod benches {
+    use super::test::{f, RECURSIONS};
+    use ndarray::prelude::*;
+    use test::Bencher;
+
+    #[bench]
+    fn spline_add_linear(b: &mut Bencher) {
+        let data: Vec<_> = Array1::linspace(0.0, 1.0, 2usize.pow(RECURSIONS))
+            .iter()
+            .map(|&x| (x, f(x)))
+            .collect();
+
+        b.iter(|| {
+            let mut spline = super::CubicHermiteSpline::empty();
+            for &(x, fx) in &data {
+                if !spline.accurate(x) {
+                    spline.add(x, fx);
+                }
+            }
+        })
+    }
+
+    #[bench]
+    fn spline_add_recursive(b: &mut Bencher) {
+        let data: Vec<_> = super::rec_linspace(0.0, 1.0, RECURSIONS)
+            .iter()
+            .map(|&x| (x, f(x)))
+            .collect();
+
+        b.iter(|| {
+            let mut spline = super::CubicHermiteSpline::empty();
+            for &(x, fx) in &data {
+                if !spline.accurate(x) {
+                    spline.add(x, fx);
+                }
+            }
+        })
+    }
+
+    #[bench]
+    fn spline_sample(b: &mut Bencher) {
+        let mut spline = super::CubicHermiteSpline::empty();
+        for x in super::rec_linspace(0.0, 1.0, RECURSIONS) {
+            if !spline.accurate(x) {
+                spline.add(x, f(x));
+            }
+        }
+
+        let xs = Array1::linspace(0.0, 1.0, 2usize.pow(RECURSIONS));
+
+        b.iter(|| {
+            for &x in &xs {
+                test::black_box(spline.sample(x));
+            }
+        })
     }
 }
