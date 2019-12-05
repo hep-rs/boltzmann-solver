0a35269c8b10e535a612179affa03dbefd101d11
JP-Ellis <josh@jpellis.me>
2019-12-05 16:04:11 +1100

    Various updates to Particle:
    - Added the HashMap for the decays
    - Remove a few `set_*` functions which don't make sense to update after the
      particle is created.
    - Fix the spin degrees of freedom to be more general
    - Use the new CubicHermiteSpline for entropy_dof
    - Update the tests
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/model/particle.rs
+++ src/model/particle.rs
@@ -4,8 +4,7 @@ use crate::{
     model::data,
     statistic::{Statistic, Statistics},
 };
-use special_functions::approximations::interpolation;
-use std::f64;
+use std::{collections::HashMap, f64};
 
 /// Particle type
 ///
@@ -22,7 +21,7 @@ use std::f64;
 /// For hashing purposes, only the particle's name is taken into account.
 ///
 /// In the long run, it is intended that this type be replaced by another one.
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct Particle {
     // The spin is stored as twice the spin, so a spin-½ particle has `spin ==
     // 1` and a spin-1 particle has `spin == 2`
@@ -35,6 +34,8 @@ pub struct Particle {
     pub width: f64,
     /// Squared width of the particle in GeV²
     pub width2: f64,
+    /// Decays
+    pub decays: HashMap<Vec<isize>, f64>,
     // Whether the particle is complex or not
     complex: bool,
     // Internal degrees of freedom of the particle
@@ -56,6 +57,7 @@ impl Particle {
             mass2: mass.powi(2),
             width,
             width2: width.powi(2),
+            decays: HashMap::new(),
             complex: false,
             dof: 1.0,
             name: "",
@@ -73,27 +75,6 @@ impl Particle {
     pub fn set_width(&mut self, width: f64) -> &mut Self {
         self.width = width;
         self.width2 = width.powi(2);
-        self
-    }
-
-    /// Indicate that the particle is real.
-    pub fn set_real(&mut self) -> &mut Self {
-        self.complex = false;
-        self
-    }
-
-    /// Indicate that the particle is complex.
-    pub fn set_complex(&mut self) -> &mut Self {
-        self.complex = true;
-        self
-    }
-
-    /// Specify how many internal degrees of freedom this particle has.
-    ///
-    /// This is a multiplicative factor to the degrees of freedom.  For a
-    /// 'pseudo' particle such as \\(B-L\\), this should be set to zero.
-    pub fn set_dof(&mut self, dof: f64) -> &mut Self {
-        self.dof = dof;
         self
     }
 
@@ -167,17 +148,23 @@ impl Particle {
     /// - spin-1/2: 2
     /// - spin-1: 2 for massless, 3 for massive
     /// - spin-3/2: 4
-    /// - spin-2: ?? for massless, 5 for massive
+    /// - spin-2: 2 for massless, 5 for massive
+    ///
+    /// or more generally, for half-integer spins `n/2`, the degrees of freedom
+    /// is `n + 1`; and for integer spin `n > 0`, the degrees of freedom are `2` for
+    /// massless or `2n + 1` for massive.
     pub fn degrees_of_freedom(&self) -> f64 {
-        // TODO: What are the massless degrees of freedom of a spin-2 particle?
         let dof = self.dof * if self.complex { 2.0 } else { 1.0 };
         match (self.spin, self.mass) {
             (0, _) => dof,
-            (1, _) => 2.0 * dof,
-            (2, x) if x == 0.0 => 2.0 * dof,
-            (2, x) if x != 0.0 => 3.0 * dof,
-            (3, _) => 4.0 * dof,
-            (4, _) => 5.0 * dof,
+            (n, _) if n % 2 == 1 => (n + 1) as f64 * dof,
+            (n, m) if n % 2 == 0 => {
+                if m == 0.0 {
+                    2.0 * dof
+                } else {
+                    (n + 1) as f64 * dof
+                }
+            }
             _ => unimplemented!("Particles with spin greater than 2 are not supported."),
         }
     }
@@ -211,11 +198,9 @@ impl Particle {
 
     pub fn entropy_dof(&self, beta: f64) -> f64 {
         if self.is_bosonic() {
-            interpolation::linear(&data::BOSON_GSTAR, (self.mass * beta).ln()).exp()
-                * self.degrees_of_freedom()
+            data::BOSON_GSTAR.sample((self.mass * beta).ln()).exp() * self.degrees_of_freedom()
         } else {
-            interpolation::linear(&data::FERMION_GSTAR, (self.mass * beta).ln()).exp()
-                * self.degrees_of_freedom()
+            data::FERMION_GSTAR.sample((self.mass * beta).ln()).exp() * self.degrees_of_freedom()
         }
     }
 }
@@ -227,15 +212,28 @@ mod tests {
 
     #[test]
     fn real_scalar() {
-        let particle = Particle::new(0, 1.0, 0.1);
+        let mut particle = Particle::new(0, 1.0, 0.1);
 
         assert!(particle.is_bosonic());
         assert!(!particle.is_fermionic());
         assert!(!particle.is_complex());
 
-        // approx_eq(particle.entropy_dof(1e-10), 1.0, 8.0, 0.0);
-        // assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(particle.entropy_dof(1e-10), 1.0, 8.0, 0.0);
+        assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(
+            particle.normalized_number_density(0.0, 1e-10),
+            1.0,
+            8.0,
+            0.0,
+        );
 
+        particle.set_mass(0.0);
+        approx_eq(
+            particle.entropy_dof(1e-10),
+            particle.entropy_dof(1e10),
+            8.0,
+            0.0,
+        );
         approx_eq(
             particle.normalized_number_density(0.0, 1e-10),
             1.0,
@@ -246,15 +244,14 @@ mod tests {
 
     #[test]
     fn complex_scalar() {
-        let mut particle = Particle::new(0, 1.0, 0.1);
-        particle.set_complex();
+        let particle = Particle::new(0, 1.0, 0.1).complex();
 
         assert!(particle.is_bosonic());
         assert!(!particle.is_fermionic());
         assert!(particle.is_complex());
 
-        // approx_eq(particle.entropy_dof(1e-10), 2.0, 8.0, 0.0);
-        // assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(particle.entropy_dof(1e-10), 2.0, 8.0, 0.0);
+        assert!(particle.entropy_dof(1e10) < 1e-30);
 
         approx_eq(
             particle.normalized_number_density(0.0, 1e-10),
@@ -272,8 +269,8 @@ mod tests {
         assert!(particle.is_fermionic());
         assert!(!particle.is_complex());
 
-        // approx_eq(particle.entropy_dof(1e-10), 2.0 * 0.875, 8.0, 0.0);
-        // assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(particle.entropy_dof(1e-10), 2.0 * 0.875, 8.0, 0.0);
+        assert!(particle.entropy_dof(1e10) < 1e-30);
 
         approx_eq(
             particle.normalized_number_density(0.0, 1e-10),
@@ -291,8 +288,8 @@ mod tests {
         assert!(!particle.is_fermionic());
         assert!(!particle.is_complex());
 
-        // approx_eq(particle.entropy_dof(1e-10), 3.0, 8.0, 0.0);
-        // assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(particle.entropy_dof(1e-10), 3.0, 8.0, 0.0);
+        assert!(particle.entropy_dof(1e10) < 1e-30);
 
         approx_eq(
             particle.normalized_number_density(0.0, 1e-10),
@@ -304,16 +301,14 @@ mod tests {
 
     #[test]
     fn complex_scalar_dof() {
-        let mut particle = Particle::new(0, 1.0, 0.1);
-        particle.set_complex();
-        particle.set_dof(2.5);
+        let particle = Particle::new(0, 1.0, 0.1).complex().dof(2.5);
 
         assert!(particle.is_bosonic());
         assert!(!particle.is_fermionic());
         assert!(particle.is_complex());
 
-        // approx_eq(particle.entropy_dof(1e-10), 5.0, 8.0, 0.0);
-        // assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(particle.entropy_dof(1e-10), 5.0, 8.0, 0.0);
+        assert!(particle.entropy_dof(1e10) < 1e-30);
 
         approx_eq(
             particle.normalized_number_density(0.0, 1e-10),
@@ -325,15 +320,14 @@ mod tests {
 
     #[test]
     fn fermion_dof() {
-        let mut particle = Particle::new(1, 1.0, 0.1);
-        particle.set_dof(1.2);
+        let particle = Particle::new(1, 1.0, 0.1).dof(1.2);
 
         assert!(!particle.is_bosonic());
         assert!(particle.is_fermionic());
         assert!(!particle.is_complex());
 
-        // approx_eq(particle.entropy_dof(1e-10), 2.0 * 1.2 * 0.875, 8.0, 0.0);
-        // assert!(particle.entropy_dof(1e10) < 1e-30);
+        approx_eq(particle.entropy_dof(1e-10), 2.0 * 1.2 * 0.875, 8.0, 0.0);
+        assert!(particle.entropy_dof(1e10) < 1e-30);
 
         approx_eq(
             particle.normalized_number_density(0.0, 1e-10),
