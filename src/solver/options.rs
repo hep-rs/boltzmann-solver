7586ad64037c05b61d7e577b2ca9d083b68bd52d
JP-Ellis <josh@jpellis.me>
2020-01-28 14:11:38 +0800

    Begin implementing serde, display and debug
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/solver/options.rs
+++ src/solver/options.rs
@@ -1,4 +1,9 @@
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
 /// Determines the range of step sizes allowed: `min * beta < h < max * beta`.
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub(crate) struct StepPrecision {
     pub min: f64,
     pub max: f64,
@@ -10,5 +15,15 @@ impl Default for StepPrecision {
             min: 1e-10,
             max: 1e-0,
         }
+    }
+}
+
+impl std::fmt::Display for StepPrecision {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "StepPrecision {{ min: {}, max: {} }}",
+            self.min, self.max
+        )
     }
 }
