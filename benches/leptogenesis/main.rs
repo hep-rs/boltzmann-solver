4599520280093f7f6a08033d2d833c585baa4d29
JP-Ellis <josh@jpellis.me>
2020-02-18 14:55:29 +1100

    Fix benchmarks

--- benches/leptogenesis/main.rs
+++ benches/leptogenesis/main.rs
@@ -12,13 +12,50 @@ use boltzmann_solver::prelude::*;
 #[cfg(feature = "nightly")]
 use test::{black_box, Bencher};
 
+/// Box an interaction
+#[cfg(feature = "parallel")]
+fn into_interaction_box<I, M>(interaction: I) -> Box<dyn Interaction<M> + Sync>
+where
+    I: Interaction<M> + Sync + 'static,
+    M: Model,
+{
+    Box::new(interaction)
+}
+
+/// Box an interaction
+#[cfg(not(feature = "parallel"))]
+fn into_interaction_box<I, M>(interaction: I) -> Box<dyn Interaction<M>>
+where
+    I: Interaction<M> + 'static,
+    M: Model,
+{
+    Box::new(interaction)
+}
+
+/// Filter iteraction based whether they involve first-generation particles only
+/// or not.
+fn one_generation<I, M>(interaction: &I) -> bool
+where
+    I: Interaction<M>,
+    M: Model,
+{
+    let ptcl = interaction.particles_idx();
+    ptcl.incoming.iter().chain(&ptcl.outgoing).all(|i| match i {
+        1 | 2 | 3 | 4 | 5 | 8 | 11 | 14 | 17 | 20 => true,
+        _ => false,
+    })
+}
+
 #[bench]
 pub fn decay_only_1gen(b: &mut Bencher) {
     b.iter(|| {
         let mut model = LeptogenesisModel::zero();
-        model
-            .interactions
-            .push(Box::new(interaction::hln().remove(0)));
+        model.interactions.extend(
+            interaction::hln()
+                .drain(..)
+                .filter(one_generation)
+                .map(into_interaction_box),
+        );
         let no_asymmetry: Vec<usize> = (0..3)
             .map(|i| LeptogenesisModel::particle_idx("N", i).unwrap())
             .collect();
@@ -38,11 +75,9 @@ pub fn decay_only_1gen(b: &mut Bencher) {
 pub fn decay_only_3gen(b: &mut Bencher) {
     b.iter(|| {
         let mut model = LeptogenesisModel::zero();
-        model.interactions.extend(
-            interaction::hln()
-                .drain(..)
-                .map(|i| Box::new(i) as Box<dyn Interaction<LeptogenesisModel> + Sync>),
-        );
+        model
+            .interactions
+            .extend(interaction::hln().drain(..).map(into_interaction_box));
         let no_asymmetry: Vec<usize> = (0..3)
             .map(|i| LeptogenesisModel::particle_idx("N", i).unwrap())
             .collect();
