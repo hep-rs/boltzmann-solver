a05e6e73c1966cebd823dc456c9391591e9fa679
JP-Ellis <josh@jpellis.me>
2020-07-08 20:12:44 +1000

    Expore `sm_data` within prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,9 @@
 //! Common imports for this crate.
 
 pub use crate::{
-    model::{interaction::Interaction, Model, ModelInteractions, Particle, StandardModel},
+    model::{
+        interaction::Interaction, standard_model::data as sm_data, Model, ModelInteractions,
+        Particle, StandardModel,
+    },
     solver::{Context, Solver, SolverBuilder},
 };
