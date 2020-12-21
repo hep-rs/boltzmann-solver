9c9e94afd4a4da6ecc51998210f34daa26417fe9
JP-Ellis <josh@jpellis.me>
2020-12-21 16:23:52 +1100

    Convert ipynb to py files
    Also update common.py

---
+++ python/full.py
@@ -0,0 +1,270 @@
+# %%
+import glob
+from pathlib import Path
+from tempfile import gettempdir
+
+from python.common import *
+
+# Find the default output directory
+OUTPUT_DIR = Path(gettempdir()) / "boltzmann_solver" / "full"
+if not OUTPUT_DIR.is_dir():
+    OUTPUT_DIR = Path("/media/ssh/uni-josh/tmp/josh/boltzmann_solver/full")
+if not OUTPUT_DIR.is_dir():
+    OUTPUT_DIR = Path("/media/ssh/bakerlab11/tmp/jellis/boltzmann_solver/full")
+display(HTML(f"<p>Loading data from <code>{OUTPUT_DIR}</p>"))
+
+# %% [markdown]
+# # Decay Only
+# %% [markdown]
+# ## 1 Generation
+
+# %%
+data = read_number_density(OUTPUT_DIR / "decay_1.csv")
+
+display(plot_integration(data))
+display(plot_densities(data, ["H", "L1", "N1"]))
+
+# %% [markdown]
+# ## 3 Generations
+
+# %%
+data = read_number_density(OUTPUT_DIR / "decay_3.csv")
+
+display(plot_integration(data))
+display(plot_densities(data, ["H", "L1", "L2", "L3", "N1", "N2", "N3"]))
+
+# %% [markdown]
+# # Washout Only
+# %% [markdown]
+# ## 1 Generation
+
+# %%
+data = read_csv(OUTPUT_DIR / "washout_1.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+plot_asymmetry(data, ["H", "L1", "N1"])
+
+
+# %%
+plot_density(data, ["H", "L1", "N1"])
+
+# %% [markdown]
+# ## 3 Generation
+
+# %%
+data = read_csv(OUTPUT_DIR / "washout_3.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+plot_asymmetry(data, ["H", "L1", "L2", "L3", "N1", "N2", "N3"])
+
+
+# %%
+plot_density(data, ["H", "L1", "L2", "L3", "N1", "N2", "N3"])
+
+# %% [markdown]
+# # Full
+# %% [markdown]
+# ## 1 Generation
+
+# %%
+data = read_csv(OUTPUT_DIR / "full_1.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+plot_asymmetry(data, ["H", "L1", "N1"])
+
+
+# %%
+plot_density(data, ["H", "L1", "N1"])
+
+# %% [markdown]
+# ## 3 Generation
+
+# %%
+data = read_csv(OUTPUT_DIR / "full_3.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+plot_asymmetry(data, ["H", "L1", "L2", "L3", "N1", "N2", "N3"])
+
+
+# %%
+plot_density(data, ["H", "L1", "L2", "L3", "N1", "N2", "N3"])
+
+# %% [markdown]
+# # Miscellaneous
+# %% [markdown]
+# ## Evolution
+
+# %%
+data, ptcls = read_evolution(OUTPUT_DIR / "evolution.json")
+
+
+# %%
+go.Figure(
+    data=[go.Scatter(name=p, x=data["beta"], y=data[p, "mass"]) for p in ptcls],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(title="Mass [GeV]", type="log", exponentformat="power"),
+    ),
+)
+
+
+# %%
+go.Figure(
+    data=[
+        go.Scatter(name=p, x=data["beta"], y=data[p, "mass"] * data["beta"])
+        for p in ptcls
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(
+            title="Mass / Temperatre", type="log", exponentformat="power"
+        ),
+    ),
+)
+
+
+# %%
+go.Figure(
+    data=[
+        go.Scatter(name=p, x=data["beta"], y=data[p, "width"] / data[p, "mass"])
+        for p in ptcls
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(title="Width / Mass", type="log", exponentformat="power"),
+    ),
+)
+
+# %% [markdown]
+# ## Higgs Equilibrium
+
+# %%
+datas = list(
+    map(read_csv, sorted(glob.glob(str(OUTPUT_DIR / "higgs_equilibrium" / "*.csv"))))
+)
+
+
+# %%
+go.Figure(
+    data=[
+        go.Scatter(
+            x=data["n"]["beta"],
+            y=data["n"]["H"],
+            mode="lines",
+            line=go.scatter.Line(color=cmap("viridis", i / len(datas))),
+            showlegend=False,
+        )
+        for i, data in enumerate(datas)
+    ]
+    + [
+        go.Scatter(
+            x=data["n"]["beta"],
+            y=data["n"]["(H)"],
+            mode="lines",
+            line=go.scatter.Line(color="black"),
+            showlegend=False,
+        )
+        for i, data in enumerate(datas)
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(title="Width / Mass", type="log", exponentformat="power"),
+    ),
+)
+
+# %% [markdown]
+# ## Lepton Equilibrium
+
+# %%
+datas = list(
+    map(read_csv, sorted(glob.glob(str(OUTPUT_DIR / "lepton_equilibrium" / "*.csv"))))
+)
+
+
+# %%
+go.Figure(
+    data=[
+        go.Scatter(
+            x=data["n"]["beta"],
+            y=data["n"]["L1"],
+            mode="lines",
+            line=go.scatter.Line(color=cmap("viridis", i / len(datas))),
+            showlegend=False,
+        )
+        for i, data in enumerate(datas)
+    ]
+    + [
+        go.Scatter(
+            x=data["n"]["beta"],
+            y=data["n"]["(L1)"],
+            mode="lines",
+            line=go.scatter.Line(color="black"),
+            showlegend=False,
+        )
+        for i, data in enumerate(datas)
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(title="Width / Mass", type="log", exponentformat="power"),
+    ),
+)
+
+# %% [markdown]
+# ## Gammas
+
+# %%
+data = pd.read_csv(OUTPUT_DIR / "gamma.csv")
+data.drop(
+    columns=list(filter(lambda x: "3" in x or "2" in x, data.columns)),
+    inplace=True,
+)
+# print(data.columns)
+plot_gamma(data)
+
+
+# %%
+data = pd.read_csv(OUTPUT_DIR / "asymmetry.csv")
+plot_gamma(data)
+
+
+# %%
---
+++ python/simple.py.py
@@ -0,0 +1,215 @@
+# %%
+import gc
+import glob
+import json
+import random
+from collections import defaultdict
+from pathlib import Path
+from tempfile import gettempdir
+
+import numpy as np
+import pandas as pd
+import plotly
+from plotly import graph_objects as go
+import scipy as sp
+import scipy.constants
+from IPython.core.display import HTML, display
+from numpy import ma
+
+from common import *
+
+
+# %%
+# Setup plotting
+
+COLORS = plotly.colors.DEFAULT_PLOTLY_COLORS
+
+
+# %%
+# Find the default output directory
+OUTPUT_DIR = Path(gettempdir()) / "boltzmann_solver" / "simple"
+if not OUTPUT_DIR.is_dir():
+    OUTPUT_DIR = Path("/media/ssh/uni-josh/tmp/josh/boltzmann_solver/simple")
+if not OUTPUT_DIR.is_dir():
+    OUTPUT_DIR = Path("/media/ssh/bakerlab11/tmp/jellis/boltzmann_solver/simple")
+print(f"Loading data from {OUTPUT_DIR}")
+
+# %% [markdown]
+# # N1F1
+
+# %%
+data = read_csv(OUTPUT_DIR / "n1f1.csv")
+# ul = pd.read_csv("/tmp/josh/ulysses/n1f1.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+fig = plot_asymmetry(data, ["H", "L1", "N1"])
+fig.update_yaxes(range=[-30, 0])
+fig.add_trace(go.Scatter(x=ul["beta"], y=ul["BL"].abs(), name="[U] B-L"))
+fig
+
+
+# %%
+fig = plot_density(data, ["H", "L1", "N1"])
+fig.add_trace(go.Scatter(x=ul["beta"], y=ul["N1"], name="[U] N1"))
+fig.add_trace(go.Scatter(x=ul["beta"], y=ul["N1eq"], name="[U] N1eq"))
+# fig.update_yaxes(type="log", range=[-20,1])
+fig
+
+# %% [markdown]
+# # N1F3
+
+# %%
+data = read_csv(OUTPUT_DIR / "n1f3.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+plot_asymmetry(data, ["H", "L1", "L2", "L3", "N1"])
+
+
+# %%
+fig = plot_density(data, ["N1"])
+fig.update_yaxes(range=[0, 2])
+
+# %% [markdown]
+# # N3F3
+
+# %%
+data = read_csv(OUTPUT_DIR / "n3f3.csv")
+
+print("Integration steps:", len(data["n"].index))
+print("Final B-L:", data["n"]["ΔB-L"].iloc[-1])
+plot_integration(data)
+
+
+# %%
+plot_asymmetry(data, ["H", "L1", "L2", "L3", "N1", "N2", "N3"])
+
+
+# %%
+fig = plot_density(data, ["N1", "N2", "N3"])
+fig.update_yaxes(range=[0, 2])
+
+# %% [markdown]
+# # Miscellaneous
+# %% [markdown]
+# ## Evolution
+
+# %%
+data, ptcls = read_evolution(OUTPUT_DIR / "evolution.json")
+
+
+# %%
+go.Figure(
+    data=[go.Scatter(name=p, x=data["beta"], y=data[p, "mass"]) for p in ptcls],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(title="Mass [GeV]", type="log", exponentformat="power"),
+    ),
+)
+
+
+# %%
+go.Figure(
+    data=[
+        go.Scatter(name=p, x=data["beta"], y=data[p, "mass"] * data["beta"])
+        for p in ptcls
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(
+            title="Mass / Temperatre", type="log", exponentformat="power"
+        ),
+    ),
+)
+
+
+# %%
+go.Figure(
+    data=[
+        go.Scatter(name=p, x=data["beta"], y=data[p, "width"] / data[p, "mass"])
+        for p in ptcls
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(
+            title="Width / Mass", type="linear", exponentformat="power"
+        ),
+    ),
+)
+
+# %% [markdown]
+# ## Gammas
+
+# %%
+data = dict()
+data["u"] = pd.read_csv("/tmp/josh/ulysses/n1f1.csv")
+data["b"] = pd.read_csv(OUTPUT_DIR / "gamma.csv")
+go.Figure(
+    data=[
+        go.Scatter(name="[U] decay", x=data["u"]["beta"], y=data["u"]["d1"]),
+        go.Scatter(name="[U] washout", x=data["u"]["beta"], y=data["u"]["w1"]),
+        go.Scatter(name="[B] decay", x=data["b"]["beta"], y=data["b"]["N1 ↔ L1 H"]),
+        go.Scatter(name="[B] washout", x=data["b"]["beta"], y=data["b"]["L1 ↔ N1"]),
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(
+            title="Interaction Rate",
+            type="log",
+            range=[-30, 15],
+            exponentformat="power",
+        ),
+    ),
+)
+
+
+# %%
+data = dict()
+data["u"] = pd.read_csv("/tmp/josh/ulysses/n1f1.csv")
+data["b"] = pd.read_csv(OUTPUT_DIR / "asymmetry.csv")
+go.Figure(
+    data=[
+        go.Scatter(name="[U] decay", x=data["u"]["beta"], y=data["u"]["epsd1"].abs()),
+        go.Scatter(
+            name="[B] decay", x=data["b"]["beta"], y=data["b"]["N1 ↔ L1 H"].abs()
+        ),
+    ],
+    layout=go.Layout(
+        xaxis=go.layout.XAxis(
+            title="Inverse Temperature [GeV⁻¹]",
+            type="log",
+            exponentformat="power",
+        ),
+        yaxis=go.layout.YAxis(
+            title="Interaction Rate", type="log", range=[-40, 5], exponentformat="power"
+        ),
+    ),
+)
+
+
+# %%
