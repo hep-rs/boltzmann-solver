76ff0c7fb633b932a300bf4f9d7b76e5e8a5703a
JP-Ellis <josh@jpellis.me>
2021-02-01 14:18:07 +1100

    Initial commit of Mathematica package

---
+++ mathematica/BoltzmannSolver/BoltzmannSolver.m
@@ -0,0 +1,22 @@
+(* Wolfram Language Package *)
+
+BeginPackage["BoltzmannSolver`"];
+
+$BSVersionMajor = 1;
+$BSVersionMinor = 0;
+$BSVersionPatch = 0;
+$BSVersion = {$BSVersionMajor, $BSVersionMinor, $BSVersionPath};
+
+Protect[$BSVersionMajor, $BSVersionMinor, $BSVersionPath];
+
+
+Get["BoltzmannSolver`Extra`"];
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+End[]
+
+EndPackage[]
+
