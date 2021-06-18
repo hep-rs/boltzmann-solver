43221242682ce7993f2b901ed6d4e5947d22f5b3
JP-Ellis <josh@jpellis.me>
2021-06-18 10:21:02 +1000

    Improve empty_model tests

--- src/model/empty_model/tests.rs
+++ src/model/empty_model/tests.rs
@@ -47,6 +47,7 @@ fn no_interaction() -> Result<(), Box<dyn error::Error>> {
     let (n, na) = SolverBuilder::new()
         .model(model)
         .beta_range(BETA_START, 1e1)
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -90,6 +91,7 @@ fn unit_amplitude() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
             csv.flush().unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -99,7 +101,7 @@ fn unit_amplitude() -> Result<(), Box<dyn error::Error>> {
     approx_eq(n0[3], 1.0, 4.0, 1e-50)?;
 
     // Check final number densities
-    approx_eq(n[1], 0.0, 4.0, 1e-10)?;
+    approx_eq(n[1], 0.0, 4.0, 1e-8)?;
     approx_eq(n[2], 2.0, 4.0, 1e-50)?;
     approx_eq(n[3], 2.0, 4.0, 1e-50)?;
 
@@ -150,7 +152,6 @@ impl Interaction<EmptyModel> for UnitGamma {
 #[test]
 fn unit_gamma() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
-
     model.extend_particles(
         [1e5, 1e2, 1e1]
             .iter()
@@ -180,6 +181,7 @@ fn unit_gamma() -> Result<(), Box<dyn error::Error>> {
             ))
             .unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -238,6 +240,7 @@ impl Interaction<EmptyModel> for UnitRate {
 
     fn rate(&self, _c: &Context<EmptyModel>) -> Option<RateDensity> {
         let mut rate = RateDensity::zero();
+        rate.gamma = 1.0;
         rate.symmetric = 1.0;
         Some(rate)
     }
@@ -247,7 +250,6 @@ impl Interaction<EmptyModel> for UnitRate {
 #[test]
 fn unit_rate() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
-
     model.extend_particles(
         [1e5, 1e2, 1e1]
             .iter()
@@ -272,11 +274,12 @@ fn unit_rate() -> Result<(), Box<dyn error::Error>> {
             ))
             .unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
     // Check final number densities
-    approx_eq(n[1], 0.0, 4.0, 1e-6)?;
+    approx_eq(n[1], 0.0, 4.0, 1e-4)?;
     approx_eq(n[2], 2.0, 4.0, 1e-50)?;
     approx_eq(n[3], 2.0, 4.0, 1e-50)?;
 
@@ -336,7 +339,6 @@ impl Interaction<EmptyModel> for UnitAdjRate {
 #[test]
 fn unit_adj_rate() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
-
     model.extend_particles(
         [1e5, 1e2, 1e1]
             .iter()
@@ -361,6 +363,7 @@ fn unit_adj_rate() -> Result<(), Box<dyn error::Error>> {
             ))
             .unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -468,7 +471,6 @@ impl Interaction<EmptyModel> for SinCos2 {
 #[test]
 fn sin_cos() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
-
     model.push_particle(Particle::new(0, 1.0, 1.0).name("1"));
     model.push_particle(Particle::new(0, 1.0, 1.0).name("2"));
     model.push_interaction(SinCos1::new());
@@ -491,6 +493,7 @@ fn sin_cos() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
         })
         .initial_densities(vec![(1, 0.0), (2, 1.0)])
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -587,7 +590,6 @@ impl Interaction<EmptyModel> for BrusselatorStable2 {
 #[test]
 fn brusselator_stable() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
-
     model.push_particle(Particle::new(0, 1.0, 1.0).name("1"));
     model.push_particle(Particle::new(0, 1.0, 1.0).name("2"));
     model.push_interaction(BrusselatorStable1::new());
@@ -610,6 +612,7 @@ fn brusselator_stable() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
         })
         .initial_densities(vec![(1, 1.0), (2, 1.0)])
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -705,7 +708,6 @@ impl Interaction<EmptyModel> for BrusselatorUnstable2 {
 #[test]
 fn brusselator_unstable() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
-
     model.push_particle(Particle::new(0, 1.0, 1.0).name("1"));
     model.push_particle(Particle::new(0, 1.0, 1.0).name("2"));
     model.push_interaction(BrusselatorUnstable1::new());
@@ -728,6 +730,7 @@ fn brusselator_unstable() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
         })
         .initial_densities(vec![(1, 1.0), (2, 1.0)])
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -739,16 +742,16 @@ fn brusselator_unstable() -> Result<(), Box<dyn error::Error>> {
 }
 
 #[test]
-fn chain_decay() -> Result<(), Box<dyn error::Error>> {
+fn chained_decay() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
     model.extend_particles(
-        [1e5, 1e2, 1e1, 1e7]
+        [1e7, 1e5, 1e2, 1e1]
             .iter()
             .enumerate()
             .map(|(i, &m)| Particle::new(0, m, m / 100.0).name(format!("{}", i + 1))),
     );
-    model.push_interaction(interaction::ThreeParticle::new(|_m| 1.0, 1, 2, 3));
-    model.push_interaction(interaction::ThreeParticle::new(|_m| 1.0, 4, 1, 2));
+    model.push_interaction(interaction::ThreeParticle::new(|_m| 1e0, 1, 2, 3));
+    model.push_interaction(interaction::ThreeParticle::new(|_m| 1e0, 2, 3, 4));
 
     // Get the initial conditions
     model.set_beta(BETA_START);
@@ -773,6 +776,7 @@ fn chain_decay() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
             csv.flush().unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -783,10 +787,10 @@ fn chain_decay() -> Result<(), Box<dyn error::Error>> {
     approx_eq(n0[4], 1.0, 4.0, 1e-50)?;
 
     // Check final number densities
-    approx_eq(n[1], 0.0, 4.0, 1e-10)?;
-    approx_eq(n[2], 4.0, 4.0, 1e-50)?;
-    approx_eq(n[3], 3.0, 4.0, 1e-50)?;
-    approx_eq(n[4], 0.0, 4.0, 1e-50)?;
+    approx_eq(n[1], 0.0, 4.0, 1e-3)?;
+    approx_eq(n[2], 0.0, 4.0, 1e-3)?;
+    approx_eq(n[3], 4.0, 2.0, 1e-50)?;
+    approx_eq(n[4], 3.0, 2.0, 1e-50)?;
 
     assert_eq!(na0, na);
 
@@ -838,6 +842,116 @@ fn scattering_massless() -> Result<(), Box<dyn error::Error>> {
             csv.flush().unwrap();
         })
         .initial_densities(vec![(1, n0[1]), (2, n0[2]), (3, n0[3]), (4, n0[4])])
+        .fast_interaction(true)
+        .build()?
+        .solve();
+
+    approx_eq(n[1] * n[2], n[3] * n[4], 2.0, 0.0)?;
+
+    assert_eq!(na0, na);
+
+    Ok(())
+}
+
+#[test]
+fn scattering_massless_eq_1122() -> Result<(), Box<dyn error::Error>> {
+    let mut model = EmptyModel::default();
+    model.extend_particles(
+        [0.0, 0.0, 0.0, 0.0]
+            .iter()
+            .enumerate()
+            .map(|(i, &m)| Particle::new(0, m, m / 100.0).name(format!("{}", i + 1))),
+    );
+    model.push_interaction(interaction::FourParticle::new(
+        |_m, _s, _t, _u| 1e-10,
+        1,
+        1,
+        2,
+        2,
+    ));
+
+    // Get the initial conditions
+    model.set_beta(BETA_START);
+    let c = model.as_context();
+    let (mut n0, na0) = (c.eq, c.na);
+    n0[1] = 0.4;
+
+    // Run the solver
+    let csv = create_csv("scattering_massless_eq_1122.csv", 4)?;
+    let (n, na) = SolverBuilder::new()
+        .model(model)
+        .beta_range(BETA_START, BETA_END)
+        .logger(move |c, dn, _dna| {
+            let mut csv = csv.write().unwrap();
+            if c.n.iter().any(|v| v.is_nan()) {
+                csv.flush().unwrap();
+                panic!("NaN at step {}.{}", c.step, c.substep);
+            };
+            csv.serialize((
+                c.step, c.beta, dn[1], c.n[1], c.eq[1], c.n[2], c.eq[2], c.n[3], c.eq[3], c.n[4],
+                c.eq[4],
+            ))
+            .unwrap();
+            csv.flush().unwrap();
+        })
+        .initial_densities(vec![(1, n0[1]), (2, n0[2]), (3, n0[3]), (4, n0[4])])
+        .in_equilibrium(vec![2])
+        .fast_interaction(true)
+        .build()?
+        .solve();
+
+    approx_eq(n[1] * n[2], n[3] * n[4], 2.0, 0.0)?;
+
+    assert_eq!(na0, na);
+
+    Ok(())
+}
+
+#[test]
+fn scattering_massless_eq_1234() -> Result<(), Box<dyn error::Error>> {
+    let mut model = EmptyModel::default();
+    model.extend_particles(
+        [0.0, 0.0, 0.0, 0.0]
+            .iter()
+            .enumerate()
+            .map(|(i, &m)| Particle::new(0, m, m / 100.0).name(format!("{}", i + 1))),
+    );
+    model.push_interaction(interaction::FourParticle::new(
+        |_m, _s, _t, _u| 1e-10,
+        1,
+        2,
+        3,
+        4,
+    ));
+
+    // Get the initial conditions
+    model.set_beta(BETA_START);
+    let c = model.as_context();
+    let (mut n0, na0) = (c.eq, c.na);
+    n0[1] = 0.4;
+    n0[2] = 0.8;
+
+    // Run the solver
+    let csv = create_csv("scattering_massless_eq_1234.csv", 4)?;
+    let (n, na) = SolverBuilder::new()
+        .model(model)
+        .beta_range(BETA_START, BETA_END)
+        .logger(move |c, dn, _dna| {
+            let mut csv = csv.write().unwrap();
+            if c.n.iter().any(|v| v.is_nan()) {
+                csv.flush().unwrap();
+                panic!("NaN at step {}.{}", c.step, c.substep);
+            };
+            csv.serialize((
+                c.step, c.beta, dn[1], c.n[1], c.eq[1], c.n[2], c.eq[2], c.n[3], c.eq[3], c.n[4],
+                c.eq[4],
+            ))
+            .unwrap();
+            csv.flush().unwrap();
+        })
+        .initial_densities(vec![(1, n0[1]), (2, n0[2]), (3, n0[3]), (4, n0[4])])
+        .in_equilibrium(vec![3, 4])
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -888,6 +1002,7 @@ fn scattering_m000() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
             csv.flush().unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -945,6 +1060,7 @@ fn scattering_m0m0() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
             csv.flush().unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -955,9 +1071,9 @@ fn scattering_m0m0() -> Result<(), Box<dyn error::Error>> {
     approx_eq(n0[4], 1.0, 4.0, 1e-50)?;
 
     // Check final number densities
-    approx_eq(n[1], 0.035_114_6, 4.0, 1e-10)?;
+    approx_eq(n[1], 0.05074, 4.0, 1e-4)?;
     approx_eq(n[1], n[2], 4.0, 1e-10)?;
-    approx_eq(n[3], 1.96489, 4.0, 1e-50)?;
+    approx_eq(n[3], 1.9493, 4.0, 1e-4)?;
     approx_eq(n[3], n[4], 4.0, 1e-50)?;
 
     assert_eq!(na0, na);
@@ -1005,6 +1121,7 @@ fn scattering_mm00() -> Result<(), Box<dyn error::Error>> {
             .unwrap();
             csv.flush().unwrap();
         })
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -1015,9 +1132,9 @@ fn scattering_mm00() -> Result<(), Box<dyn error::Error>> {
     approx_eq(n0[4], 1.0, 4.0, 1e-50)?;
 
     // Check final number densities
-    approx_eq(n[1], 0.275_935, 4.0, 1e-10)?;
+    approx_eq(n[1], 0.276, 4.0, 1e-4)?;
     approx_eq(n[1], n[2], 4.0, 1e-10)?;
-    approx_eq(n[3], 1.72406, 4.0, 1e-50)?;
+    approx_eq(n[3], 1.724, 4.0, 1e-4)?;
     approx_eq(n[3], n[4], 4.0, 1e-50)?;
 
     assert_eq!(na0, na);
@@ -1066,11 +1183,12 @@ fn scattering_mmmm() -> Result<(), Box<dyn error::Error>> {
             csv.flush().unwrap();
         })
         .initial_densities(vec![(1, 1.3), (2, 1.1), (3, 0.9), (4, 0.6)])
+        .fast_interaction(true)
         .build()?
         .solve();
 
     // Check final number densities
-    approx_eq(n[1] * n[2], n[3] * n[4], 4.0, 1e-10)?;
+    approx_eq(n[1] * n[2], n[3] * n[4], 2.0, 1e-5)?;
 
     assert_eq!(na0, na);
 
@@ -1078,10 +1196,10 @@ fn scattering_mmmm() -> Result<(), Box<dyn error::Error>> {
 }
 
 #[test]
-fn scattering_mmmm_eq() -> Result<(), Box<dyn error::Error>> {
+fn scattering_mm00_eq() -> Result<(), Box<dyn error::Error>> {
     let mut model = EmptyModel::default();
     model.extend_particles(
-        [1e5, 1e5, 1e5, 1e5]
+        [1e5, 1e5, 0.0, 0.0]
             .iter()
             .enumerate()
             .map(|(i, &m)| Particle::new(0, m, m / 100.0).name(format!("{}", i + 1))),
@@ -1100,7 +1218,7 @@ fn scattering_mmmm_eq() -> Result<(), Box<dyn error::Error>> {
     let (n0, na0) = (c.eq, c.na);
 
     // Run the solver
-    let csv = create_csv("scattering_mmmm_eq.csv", 4)?;
+    let csv = create_csv("scattering_mm00_eq.csv", 4)?;
     let (n, na) = SolverBuilder::new()
         .model(model)
         .beta_range(BETA_START, BETA_END)
@@ -1119,6 +1237,7 @@ fn scattering_mmmm_eq() -> Result<(), Box<dyn error::Error>> {
         })
         .initial_densities(vec![(1, 1.3), (2, 1.1)])
         .in_equilibrium(vec![3, 4])
+        .fast_interaction(true)
         .build()?
         .solve();
 
@@ -1129,7 +1248,10 @@ fn scattering_mmmm_eq() -> Result<(), Box<dyn error::Error>> {
     approx_eq(n0[4], 1.0, 4.0, 1e-50)?;
 
     // Check final number densities
-    approx_eq(n[1] * n[2], n[3] * n[4], 4.0, 1e-10)?;
+    approx_eq(n[1], 0.3772, 4.0, 1e-10)?;
+    approx_eq(n[2], 0.1772, 4.0, 1e-10)?;
+    approx_eq(n[3], 1.0, 4.0, 1e-10)?;
+    approx_eq(n[4], 1.0, 4.0, 1e-10)?;
 
     assert_eq!(na0, na);
 
