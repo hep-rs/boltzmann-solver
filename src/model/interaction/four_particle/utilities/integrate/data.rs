cef7794fa591c11e76878128cb69edbee4b6848a
JP-Ellis <josh@jpellis.me>
2022-03-25 14:06:00 +1100

    Add special integration routines
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ src/model/interaction/four_particle/utilities/integrate/data.rs
@@ -0,0 +1,6 @@
+//! Quadrature data
+
+include!("shifted_legendre.inc.rs");
+include!("bessel_pos.inc.rs");
+include!("bessel_neg.inc.rs");
+include!("bessel_zero.inc.rs");
---
+++ src/model/interaction/four_particle/utilities/integrate/laguerre.inc.rs
@@ -0,0 +1,4 @@
+pub(crate) const LAGUERRE_2: [[f64; 3]; 1] = Transpose(1.];
+
+pub(crate) const LAGUERRE_3: [[f64; 3]; 1] = Transpose(2.];
+
